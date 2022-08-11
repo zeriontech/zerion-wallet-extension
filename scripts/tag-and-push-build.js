1c49e6a79f91a1447e9d292d5620e0c18eee0679
everdimension <everdimension@gmail.com>
2022-08-11 13:25:44 +0300

    1.0.0-alpha.0

---
+++ scripts/tag-and-push-build.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+const ghPages = require('gh-pages');
+const { version } = require('../package.json');
+
+const tagName = `build-${version}`;
+
+ghPages.publish('dist', {
+  branch: 'releases',
+  tag: tagName,
+});
