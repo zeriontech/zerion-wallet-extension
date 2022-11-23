87aea2dc9b74f24fb63975b1384303710fff6453
everdimension <everdimension@gmail.com>
2022-11-23 23:28:37 +0700

    Fix sign error on 1inch (#22)
    When transaction has { type: "0x2" }, ethers throws a serialization
    error
    To fix this, type must be converted to number

--- src/modules/ethereum/types/IncomingTransaction.ts
+++ src/modules/ethereum/types/IncomingTransaction.ts
@@ -1,7 +1,11 @@
 import type { BigNumberish } from 'ethers';
 import type { UnsignedTransaction } from './UnsignedTransaction';
 
-export type IncomingTransaction = Omit<UnsignedTransaction, 'chainId'> & {
+export type IncomingTransaction = Omit<
+  UnsignedTransaction,
+  'chainId' | 'type'
+> & {
   chainId?: number | string;
   gas?: BigNumberish;
+  type?: string | number | null;
 };
