31d8fd301b527ffddb426670a9974313a338a296
everdimension <everdimension@gmail.com>
2022-12-14 16:41:43 +0700

    Add browser name, version and platform to BugReportButton params (#48)

---
+++ src/ui/components/BugReportButton/detectBrowser.test.ts
@@ -0,0 +1,33 @@
+import { detectBrowser } from './detectBrowser';
+
+describe('detectBrowser', () => {
+  const userAgents = {
+    firefox:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+    chrome:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    safari:
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1',
+  };
+
+  test('detects chrome', () => {
+    expect(detectBrowser(userAgents.chrome)).toEqual({
+      browser: 'Chrome',
+      version: '108.0.0.0',
+    });
+  });
+
+  test('detects firefox', () => {
+    expect(detectBrowser(userAgents.firefox)).toEqual({
+      browser: 'Firefox',
+      version: '107.0',
+    });
+  });
+
+  test('detects safari', () => {
+    expect(detectBrowser(userAgents.safari)).toEqual({
+      browser: 'Safari',
+      version: '604.1',
+    });
+  });
+});
---
+++ src/ui/components/BugReportButton/detectBrowser.ts
@@ -0,0 +1,21 @@
+export function detectBrowser(string: string) {
+  /**
+   * We parse as simple as possible without considering edge cases
+   * or unusual browsers; extension can only be run in Chrome anyway right now
+   */
+  const patterns = {
+    firefox: /(Firefox)\/((\w+|\.)+)/,
+    chrome: /(Chrome)\/((\w|\.)+)/,
+    safari: /(Safari)\/((\w|\.)+)/,
+  };
+  const match =
+    string.match(patterns.firefox) ||
+    string.match(patterns.chrome) ||
+    string.match(patterns.safari);
+
+  if (match) {
+    return { browser: match[1], version: match[2] };
+  } else {
+    return { browser: 'unknown', version: 'unknown' };
+  }
+}
