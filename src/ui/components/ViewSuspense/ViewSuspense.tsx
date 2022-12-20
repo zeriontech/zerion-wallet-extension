7feb600d9b16f6598e1cd5380692d94ed769e792
everdimension <everdimension@gmail.com>
2022-12-20 13:08:38 +0700

    Avoid title flickering on route change (#55)

--- src/ui/components/ViewSuspense/ViewSuspense.tsx
+++ src/ui/components/ViewSuspense/ViewSuspense.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { DelayedRender } from '../DelayedRender';
+import { NavigationTitle } from '../NavigationTitle';
 import { ViewLoading } from '../ViewLoading';
 
 export function ViewSuspense({ children }: React.PropsWithChildren) {
   return (
     <React.Suspense
       fallback={
-        <DelayedRender>
-          <ViewLoading />
-        </DelayedRender>
+        <>
+          {/* empty navigation title to avoid flickering during route change */}
+          <NavigationTitle title={null} />
+          <DelayedRender>
+            <ViewLoading />
+          </DelayedRender>
+        </>
       }
     >
       {children}
