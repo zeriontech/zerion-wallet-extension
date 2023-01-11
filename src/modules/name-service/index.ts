be93e0d135f3f12173f355c12c2470e72cc61ddb
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-01-12 02:37:09 +0300

    Use allSettled

--- src/modules/name-service/index.ts
+++ src/modules/name-service/index.ts
@@ -6,9 +6,14 @@ export type Registry = (address: string) => Promise<string | null>;
 export const registries = [ensLookup, lensLookup];
 
 async function lookupAddressNames(address: string): Promise<string[]> {
-  const addresses = await Promise.all(
+  const addresses = await Promise.allSettled(
     registries.map((lookup: Registry) => lookup(address))
-  );
+  ).then((results) => {
+    const fulfilled = results.filter(
+      (res) => res.status === 'fulfilled'
+    ) as PromiseFulfilledResult<string>[];
+    return fulfilled.map((res) => res.value);
+  });
   return addresses.filter((address): address is string => address !== null);
 }
 
