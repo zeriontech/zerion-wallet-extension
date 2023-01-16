af800374994305f9d96085e2014ef216a58259b3
everdimension <everdimension@gmail.com>
2023-01-16 20:30:35 +0300

    Update ViewLoading spinner to indicate a request that's taking too long (#74)

--- src/ui/components/ViewLoading/ViewLoading.tsx
+++ src/ui/components/ViewLoading/ViewLoading.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { ellipsis, NBSP } from 'src/ui/shared/typography';
 import { CircleSpinner } from 'src/ui/ui-kit/CircleSpinner';
 import { UIText } from 'src/ui/ui-kit/UIText';
+import { VStack } from 'src/ui/ui-kit/VStack';
+import { useRenderDelay } from '../DelayedRender/DelayedRender';
 import { FillView } from '../FillView';
 
 interface Props {
@@ -9,6 +12,7 @@ interface Props {
 }
 
 export function ViewLoading({ size = '24px', kind = 'default' }: Props) {
+  const isTooLong = useRenderDelay(6000);
   return (
     <FillView>
       {!navigator.onLine && kind === 'network' ? (
@@ -16,7 +20,17 @@ export function ViewLoading({ size = '24px', kind = 'default' }: Props) {
           You are offline
         </UIText>
       ) : (
-        <CircleSpinner color="var(--primary)" size={size} />
+        <VStack gap={8} style={{ placeItems: 'center' }}>
+          <CircleSpinner color="var(--primary)" size={size} />
+
+          {kind === 'network' ? (
+            <UIText kind="small/regular" color="var(--neutral-600)">
+              {isTooLong
+                ? `Request is taking longer than usual${ellipsis}`
+                : NBSP}
+            </UIText>
+          ) : null}
+        </VStack>
       )}
     </FillView>
   );
