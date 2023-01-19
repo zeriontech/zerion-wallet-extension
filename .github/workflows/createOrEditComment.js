97f5b19a0570712cc0f4fca8343daf28c4373b2b
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-01-19 12:28:54 +0300

    Update build comment

--- .github/workflows/createOrEditComment.js
+++ .github/workflows/createOrEditComment.js
@@ -1,24 +1,17 @@
 /* eslint-env node */
 /* eslint-disable import/no-commonjs, import/no-nodejs-modules */
 
-const formatDate = (date) =>
-  new Intl.DateTimeFormat('en', {
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  }).format(date);
+import fs from 'fs';
 
-module.exports = async ({ github, context, pattern, message }) => {
+module.exports = async ({ github, context, pattern, filePath }) => {
   const { data: comments } = await github.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
   });
   const comment = comments.find((c) => c.body.startsWith(pattern));
-
   const repo = { owner: context.repo.owner, repo: context.repo.repo };
+  const body = fs.readFileSync(filePath, 'utf8');
 
   const updateComment = ({ comment_id, body }) =>
     github.issues.updateComment({ ...repo, comment_id, body });
@@ -26,9 +19,8 @@ module.exports = async ({ github, context, pattern, message }) => {
     github.issues.createComment({ ...repo, issue_number, body });
 
   if (comment) {
-    const body = `${message} [updated at ${formatDate(new Date())} UTC]`;
     updateComment({ comment_id: comment.id, body });
   } else {
-    createComment({ issue_number: context.issue.number, body: message });
+    createComment({ issue_number: context.issue.number, body });
   }
 };
