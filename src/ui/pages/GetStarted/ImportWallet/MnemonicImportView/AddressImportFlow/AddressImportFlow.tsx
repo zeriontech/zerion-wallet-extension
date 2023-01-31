8c8676a62f4d2a42d92b8ab8d5ac14f3ffc69146
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-01-31 18:06:17 +0300

    Fix wallets ready to import screen

--- src/ui/pages/GetStarted/ImportWallet/MnemonicImportView/AddressImportFlow/AddressImportFlow.tsx
+++ src/ui/pages/GetStarted/ImportWallet/MnemonicImportView/AddressImportFlow/AddressImportFlow.tsx
@@ -98,7 +98,7 @@ function AddressImportList({
         <PageBottom />
       </PageColumn>
 
-      <PageStickyFooter lineColor="var(--neutral-300)">
+      <PageStickyFooter lineColor="var(--neutral-300)" style={{ bottom: 29 }}>
         <VStack
           style={{
             marginTop: 8,
