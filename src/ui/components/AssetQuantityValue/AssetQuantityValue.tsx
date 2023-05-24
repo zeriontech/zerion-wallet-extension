20d9179b8f3c1af373f0420887107e8c6ede5127
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-05-24 22:56:49 +0300

    Remove quantity truncation

--- src/ui/components/AssetQuantityValue/AssetQuantityValue.tsx
+++ src/ui/components/AssetQuantityValue/AssetQuantityValue.tsx
@@ -3,7 +3,6 @@ import BigNumber from 'bignumber.js';
 import React from 'react';
 import type { AssetQuantity } from 'src/modules/networks/asset';
 import { formatTokenValue } from 'src/shared/units/formatTokenValue';
-import { middleTruncate } from 'src/ui/shared/middleTruncate';
 import { muchGreater, veryMuchGreater } from 'src/ui/shared/typography';
 import memoize from 'lodash/memoize';
 
@@ -21,9 +20,7 @@ export function AssetQuantityValue({
   quantity: AssetQuantity;
 }) {
   const style: CSSProperties = {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+    wordBreak: 'break-all',
   };
 
   if (quantity.type === 'veryLarge') {
@@ -37,14 +34,6 @@ export function AssetQuantityValue({
     return <span style={style}>{`${muchGreater} 1T`}</span>;
   } else {
     const formatted = formatWithSignificantValue(quantity.value);
-    const value =
-      quantity.value.lt(new BigNumber(1)) && formatted.length > 8
-        ? middleTruncate({
-            value: quantity.value.toString(),
-            trailingLettersCount: 5,
-          })
-        : formatted;
-
-    return <span style={style}>{`${sign || ''}${value.trim()}`}</span>;
+    return <span style={style}>{`${sign || ''}${formatted.trim()}`}</span>;
   }
 }
