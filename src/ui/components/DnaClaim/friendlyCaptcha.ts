fa9c6bbcb3bd32696ae53879f9e675d54312b476
everdimension <everdimension@gmail.com>
2023-06-27 08:55:45 +0100

    Time friendly captcha computation

--- src/ui/components/DnaClaim/friendlyCaptcha.ts
+++ src/ui/components/DnaClaim/friendlyCaptcha.ts
@@ -1,8 +1,13 @@
 import { WidgetInstance } from 'friendly-challenge';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const ZERION_CAPTCHA_SITEKEY = 'FCMTM2991RKE7478';
 
 export async function resolveCaptcha() {
+  if (isDevelopment) {
+    console.time('friendly captcha solution'); // eslint-disable-line no-console
+  }
   // we need to create captcha anchor every time
   // because this element will be destroyed in successful callback
   const element = document.createElement('div');
@@ -10,9 +15,15 @@ export async function resolveCaptcha() {
   document.body.appendChild(element);
 
   return new Promise<string>((resolve) => {
+    if (isDevelopment) {
+      console.log('will solve friendly captcha'); // eslint-disable-line no-console
+    }
     const widget = new WidgetInstance(element, {
       doneCallback: (solution: string) => {
         resolve(solution);
+        if (isDevelopment) {
+          console.timeEnd('friendly captcha solution'); // eslint-disable-line no-console
+        }
         // we need to postpone destroying widget to avoid captcha internal problem
         setTimeout(() => widget?.destroy());
       },
