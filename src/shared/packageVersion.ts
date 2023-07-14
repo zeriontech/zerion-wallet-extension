d852253f0f132bf84488bffb7067f992835daae8
everdimension <everdimension@gmail.com>
2023-07-14 22:43:14 +0100

    fix NODE_ENV check

--- src/shared/packageVersion.ts
+++ src/shared/packageVersion.ts
@@ -1,5 +1,5 @@
 import { version as packageVersion } from '../../package.json';
 
-const isProd = process.env.NODE_VERSION === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 export const version = isProd ? packageVersion : `${packageVersion}-dev`;
