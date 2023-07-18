f6bbeb83970f51439d7a9870b4f535619e419f84
Andrey <zertsalov96@yandex.ru>
2023-07-18 16:01:27 +0100

    fix offset computation

--- src/ui/pages/Overview/getTabsOffset.ts
+++ src/ui/pages/Overview/getTabsOffset.ts
@@ -1,9 +1,9 @@
 export const TABS_OFFSET_METER_ID = 'overview-tabs-offset-meter';
-const TABS_HEIGHT = 47;
+export const TABS_OFFSET = 48;
 
 export function getTabsOffset() {
   return (
     (document.getElementById(TABS_OFFSET_METER_ID)?.offsetTop || 0) -
-    TABS_HEIGHT
+    TABS_OFFSET
   );
 }
