1f1fcba26443bc293b180b804601b4c0515b8fac
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-08-10 13:41:02 +0300

    Fix for nested typed data structures

--- src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
+++ src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
@@ -3,15 +3,26 @@ import { TextLine } from 'src/ui/components/address-action/TextLine';
 import { Surface } from 'src/ui/ui-kit/Surface';
 import { VStack } from 'src/ui/ui-kit/VStack';
 
-export function TypedDataAdvancedView({
-  data,
-}: {
-  data: Record<string, string>;
-}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Value = any;
+type TypedData = Record<string, Value>;
+
+function flattenObject(obj: TypedData, prefix = '') {
+  return Object.keys(obj).reduce((acc, key) => {
+    const leadingPrefix = prefix.length ? prefix + '.' : '';
+    if (typeof obj[key] === 'object')
+      Object.assign(acc, flattenObject(obj[key], `${leadingPrefix}${key}`));
+    else acc[`${leadingPrefix}${key}`] = obj[key];
+    return acc;
+  }, {} as TypedData);
+}
+
+export function TypedDataAdvancedView({ data }: { data: TypedData }) {
+  const flattenedData = flattenObject(data);
   return (
     <Surface padding={16}>
       <VStack gap={16}>
-        {Object.entries(data).map(([label, value]) => (
+        {Object.entries(flattenedData).map(([label, value]) => (
           <TextLine wrap={true} key={label} label={label} value={value} />
         ))}
       </VStack>
