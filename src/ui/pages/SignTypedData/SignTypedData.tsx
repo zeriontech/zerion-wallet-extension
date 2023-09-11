3fae3a67dfcaa1c089ee16a574b92d0ff5d3b2a1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-09-11 10:35:59 +0300

    Fix keyboard shortcut, naming

--- src/ui/pages/SignTypedData/SignTypedData.tsx
+++ src/ui/pages/SignTypedData/SignTypedData.tsx
@@ -58,6 +58,7 @@ function isPermit({ message }: TypedData) {
 enum View {
   default = 'default',
   advanced = 'advanced',
+  customAllowance = 'customAllowance',
 }
 
 function SignTypedDataContent({
@@ -83,7 +84,7 @@ function SignTypedDataContent({
     onSuccess: handleSignSuccess,
   });
 
-  const someMutationError = signTypedData_v4Mutation.isError
+  const signMutationError = signTypedData_v4Mutation.isError
     ? getError(signTypedData_v4Mutation.error)
     : null;
   const originForHref = useMemo(() => prepareForHref(origin), [origin]);
@@ -262,9 +263,9 @@ function SignTypedDataContent({
           }}
           gap={8}
         >
-          {someMutationError ? (
+          {signMutationError ? (
             <UIText kind="caption/regular" color="var(--negative-500)">
-              {someMutationError?.message}
+              {signMutationError?.message}
             </UIText>
           ) : null}
           <div
