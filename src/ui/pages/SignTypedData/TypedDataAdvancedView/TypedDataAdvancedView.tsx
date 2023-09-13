fc8d67a1e1167e6d3dae82248de840a1f184e366
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-09-13 13:22:13 +0300

    Update interpretation response handling

--- src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
+++ src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
@@ -3,39 +3,19 @@ import { NavigationBar } from 'src/ui/components/NavigationBar';
 import { TextLine } from 'src/ui/components/address-action/TextLine';
 import { Surface } from 'src/ui/ui-kit/Surface';
 import { VStack } from 'src/ui/ui-kit/VStack';
+import type { InterpretInput } from 'src/modules/ethereum/transactions/types';
 
-type Value = unknown;
-type TypedDataJSON = Record<string, Value>;
-
-function flattenObject(obj: TypedDataJSON, prefix = '') {
-  return Object.keys(obj).reduce<Record<string, string>>((acc, key) => {
-    const leadingPrefix = prefix.length ? prefix + '.' : '';
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
-      Object.assign(
-        acc,
-        flattenObject(obj[key] as TypedDataJSON, `${leadingPrefix}${key}`)
-      );
-    } else {
-      acc[`${leadingPrefix}${key}`] = String(obj[key]);
-    }
-    return acc;
-  }, {});
-}
-
-export function TypedDataAdvancedView({
-  dataJSON,
-}: {
-  dataJSON: TypedDataJSON;
-}) {
-  const flattenedData = flattenObject(dataJSON);
+export function TypedDataAdvancedView({ data }: { data: InterpretInput }) {
   return (
     <>
       <NavigationBar title="Advanced View" />
       <Surface padding={16}>
         <VStack gap={16}>
-          {Object.entries(flattenedData).map(([label, value]) => (
-            <TextLine wrap={true} key={label} label={label} value={value} />
-          ))}
+          {data.sections.flatMap(({ blocks }) =>
+            blocks.map(({ name, value }) => (
+              <TextLine wrap={true} key={name} label={name} value={value} />
+            ))
+          )}
         </VStack>
       </Surface>
     </>
