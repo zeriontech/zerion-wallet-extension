e86cf9f499326b3fc5295e2df0f36bb506ce5cf9
everdimension <everdimension@gmail.com>
2023-09-15 14:41:07 +0300

    Remove "flag as dapp" logic (#284)
    Do not pass origin as a param to wallet API,
    instead, read it from context
    
    Fixes Slow Mist Audit Issue N8

--- src/shared/dapps/index.ts
+++ src/shared/dapps/index.ts
@@ -1,36 +1,3 @@
-import * as browserStorage from 'src/background/webapis/storage';
-
-type Registry = Record<string, true>;
-
-export async function initialize() {
-  /**
-   * Refresh dapp registry on each start
-   */
-  browserStorage.remove('dappRegistry'); // currently no need to await
-}
-
-export async function flagAsDapp({
-  origin,
-}: {
-  origin: string;
-}): Promise<void> {
-  const maybeRegistry = await browserStorage.get<Registry>('dappRegistry');
-  const registry = {
-    ...maybeRegistry,
-    [origin]: true,
-  };
-  await browserStorage.set('dappRegistry', registry);
-}
-
-export async function isFlaggedAsDapp({
-  origin,
-}: {
-  origin: string;
-}): Promise<boolean> {
-  const maybeRegistry = await browserStorage.get<Registry>('dappRegistry');
-  return maybeRegistry ? maybeRegistry[origin] : false;
-}
-
 export function getNameFromOrigin(origin: string) {
   return new URL(origin).hostname;
 }
