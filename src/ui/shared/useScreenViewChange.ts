22c263b6ce16228b65b8f41e7254482e51ced077
Andrey <zertsalov96@yandex.ru>
2023-09-21 12:49:27 +0100

    fix format

--- src/ui/shared/useScreenViewChange.ts
+++ src/ui/shared/useScreenViewChange.ts
@@ -18,7 +18,7 @@ export function useScreenViewChange() {
         pathname,
         address,
         previous: previousPathname.current,
-        windowSize: `${window.screen.width} x ${window.screen.height}`,
+        windowSize: `${window.screen.width}x${window.screen.height}`,
       });
     },
     onSuccess() {
