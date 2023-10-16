e29ef50ef0584976c1c9ef730f6c4f4d201aaa98
everdimension <everdimension@gmail.com>
2023-10-16 18:55:52 +0300

    Fix spacing in TypedDataAdvancedView

--- src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
+++ src/ui/pages/SignTypedData/TypedDataAdvancedView/TypedDataAdvancedView.tsx
@@ -4,11 +4,13 @@ import { TextLine } from 'src/ui/components/address-action/TextLine';
 import { Surface } from 'src/ui/ui-kit/Surface';
 import { VStack } from 'src/ui/ui-kit/VStack';
 import type { InterpretInput } from 'src/modules/ethereum/transactions/types';
+import { PageTop } from 'src/ui/components/PageTop';
 
 export function TypedDataAdvancedView({ data }: { data: InterpretInput }) {
   return (
     <>
       <NavigationBar title="Advanced View" />
+      <PageTop />
       <Surface padding={16}>
         <VStack gap={16}>
           {data.sections.flatMap(({ blocks }) =>
