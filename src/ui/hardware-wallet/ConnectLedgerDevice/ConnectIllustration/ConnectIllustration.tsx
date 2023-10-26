7fcbe0349d68c36ad18c4a572523c3c04c218fff
everdimension <everdimension@gmail.com>
2023-10-26 20:38:15 +0300

    Update ledger animation

--- src/ui/hardware-wallet/ConnectLedgerDevice/ConnectIllustration/ConnectIllustration.tsx
+++ src/ui/hardware-wallet/ConnectLedgerDevice/ConnectIllustration/ConnectIllustration.tsx
@@ -7,22 +7,35 @@ import * as styles from './styles.module.css';
 
 export function ConnectIllustration() {
   return (
-    <HStack
-      gap={0}
-      justifyContent="space-between"
-      alignItems="center"
-      style={{ overflow: 'hidden' }}
-    >
+    <HStack gap={0} justifyContent="space-between" alignItems="center">
       <LedgerWire
         style={{
           animation: `${styles.connectWire} 2s`,
           animationDelay: '1s',
           animationFillMode: 'backwards',
           animationTimingFunction: 'ease-in-out',
+          zIndex: 1,
         }}
       />
-      <div style={{ position: 'relative', left: -4 }}>
-        <LedgerBody />
+      <div style={{ position: 'relative', left: -4, zIndex: 1 }}>
+        <svg
+          style={{
+            position: 'absolute',
+            zIndex: -1,
+            left: -50,
+            top: -50 + 60 / 2,
+            width: 100,
+            height: 100,
+            animation: `${styles.pulse} 2s`,
+            animationIterationCount: 3,
+            animationDelay: '3.5s',
+            animationFillMode: 'both',
+          }}
+          viewBox="0 0 100 100"
+        >
+          <circle r="50" cx="50" cy="50" fill="var(--neutral-700)" />
+        </svg>
+        <LedgerBody style={{ zIndex: 1 }} />
         <div
           style={{
             position: 'absolute',
