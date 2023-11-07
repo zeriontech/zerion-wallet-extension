63060cf8dd757aee5a61fb6206c8b225fdc3a4ab
everdimension <everdimension@gmail.com>
2023-11-07 16:50:44 +0300

    Install @zeriontech/components (#318)

---
+++ src/ui/ui-kit/Toggle.ts
@@ -0,0 +1,2 @@
+import '@zeriontech/components/lib/style.css';
+export { Toggle } from '@zeriontech/components/lib/Toggle';
---
+++ src/ui/ui-kit/UIText.ts
@@ -0,0 +1,3 @@
+import '@zeriontech/components/lib/style.css';
+export { UIText, uiTextParams } from '@zeriontech/components/lib/UIText';
+export type { UITextProps, Kind } from '@zeriontech/components/lib/UIText';
