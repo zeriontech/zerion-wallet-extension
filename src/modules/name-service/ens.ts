d367f0b8805095dedc7b5eab4981ff6fb3acb445
everdimension <everdimension@gmail.com>
2023-11-24 18:27:22 +0300

    Reduce number of RPC calls made by ens lookups
    TODO: We should switch to our internal lookup endpoint

--- src/modules/name-service/ens.ts
+++ src/modules/name-service/ens.ts
@@ -1,14 +1,23 @@
 import { ethers } from 'ethers';
 import { networksStore } from 'src/modules/networks/networks-store.client';
 import { ChainId } from 'src/modules/ethereum/transactions/ChainId';
+import memoize from 'lodash/memoize';
+
+const getRpcProvider = memoize(
+  (url) => new ethers.providers.JsonRpcProvider(url)
+);
+
+const lookup = memoize((address, url) => {
+  const provider = getRpcProvider(url);
+  return provider.lookupAddress(address);
+});
 
 export async function ensLookup(address: string): Promise<string | null> {
   const networks = await networksStore.load();
   const nodeUrl = networks.getRpcUrlInternal(
     networks.getChainById(ChainId.Mainnet)
   );
-  const provider = new ethers.providers.JsonRpcProvider(nodeUrl);
-  return provider.lookupAddress(address);
+  return lookup(address, nodeUrl);
 }
 
 function ensMatch(maybeDomain: string) {
@@ -23,6 +32,6 @@ export async function ensResolve(domain: string): Promise<string | null> {
   const nodeUrl = networks.getRpcUrlInternal(
     networks.getChainById(ChainId.Mainnet)
   );
-  const provider = new ethers.providers.JsonRpcProvider(nodeUrl);
+  const provider = getRpcProvider(nodeUrl);
   return provider.resolveName(domain);
 }
