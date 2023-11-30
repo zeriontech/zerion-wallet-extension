e0292a57f6b54899a5f19b7c3c39bab171c0a0a6
Andrey <zertsalov96@yandex.ru>
2023-11-30 14:25:00 +0000

    remove unused code

--- src/modules/remote-config/types.ts
+++ src/modules/remote-config/types.ts
@@ -4,5 +4,4 @@ export interface RemoteConfig {
   user_can_create_initial_wallet: boolean;
   extension_wallet_name_flags: Record<string, WalletNameFlag[]>;
   extension_invitation_campaign_id: string;
-  extension_show_footer_bug_button: boolean;
 }
