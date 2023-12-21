d9c2125300421d1f121f85c193ce01f1a7c1ad48
Andrey <zertsalov2910@gmail.com>
2023-12-21 08:58:14 +0000

    Add Ledger icon to wallet avatars (#394)

--- src/ui/components/WalletAvatar/WalletAvatar.tsx
+++ src/ui/components/WalletAvatar/WalletAvatar.tsx
@@ -7,25 +7,34 @@ export function WalletAvatar({
   address,
   size,
   borderRadius = 6,
+  icon,
 }: {
   active?: boolean;
   address: string;
   size: number;
   borderRadius?: number;
+  icon?: React.ReactNode;
 }) {
   const { data: nft, isLoading } = useProfileNft(address);
 
   if (isLoading) {
-    return <div style={{ width: size, height: size }} />;
+    return (
+      <div style={{ width: size, height: size, position: 'relative' }}>
+        {icon}
+      </div>
+    );
   }
 
   return (
-    <AvatarIcon
-      active={active}
-      address={address}
-      size={size}
-      nft={nft}
-      borderRadius={borderRadius}
-    />
+    <div style={{ position: 'relative' }}>
+      <AvatarIcon
+        active={active}
+        address={address}
+        size={size}
+        nft={nft}
+        borderRadius={borderRadius}
+      />
+      {icon}
+    </div>
   );
 }
