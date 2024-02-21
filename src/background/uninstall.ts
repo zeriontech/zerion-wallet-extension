07e89afdc1e5087a20b3665eb9cbba575a6d675c
Andrey <zertsalov2910@gmail.com>
2024-02-21 17:58:56 +0000

    Add extension version to uninstall poll link (#420)

--- src/background/uninstall.ts
+++ src/background/uninstall.ts
@@ -1,13 +1,18 @@
 import browser from 'webextension-polyfill';
 import type { RemoteConfig } from 'src/modules/remote-config';
 import { getRemoteConfigValue } from 'src/modules/remote-config';
+import { version } from 'src/shared/packageVersion';
 
 export function setUninstallURL() {
-  const uninstallURL = getRemoteConfigValue(
+  const uninstallLink = getRemoteConfigValue(
     'extension_uninstall_link'
   ) as RemoteConfig['extension_uninstall_link'];
 
-  if (uninstallURL) {
-    browser.runtime.setUninstallURL(uninstallURL);
+  if (uninstallLink) {
+    const uninstallURL = new URL(uninstallLink);
+    uninstallURL.hash = uninstallURL.hash
+      ? `${uninstallURL.hash}&app_version=${version}`
+      : `#app_version=${version}`;
+    browser.runtime.setUninstallURL(uninstallURL.toString());
   }
 }
