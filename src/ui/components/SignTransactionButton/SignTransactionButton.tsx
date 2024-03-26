1071ffd77398a74c93905f803597899d36b96032
everdimension <everdimension@gmail.com>
2024-03-26 17:20:05 +0800

    Warning dialog for message signing and updated analytics

--- src/ui/components/SignTransactionButton/SignTransactionButton.tsx
+++ src/ui/components/SignTransactionButton/SignTransactionButton.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import type { ethers } from 'ethers';
 import React, { useImperativeHandle, useRef } from 'react';
 import type { IncomingTransaction } from 'src/modules/ethereum/types/IncomingTransaction';
@@ -6,19 +6,14 @@ import { createChain } from 'src/modules/networks/Chain';
 import { invariant } from 'src/shared/invariant';
 import type { ExternallyOwnedAccount } from 'src/shared/types/ExternallyOwnedAccount';
 import type { TransactionContextParams } from 'src/shared/types/SignatureContextParams';
-import {
-  isDeviceAccount,
-  isReadonlyContainer,
-} from 'src/shared/types/validators';
+import { isDeviceAccount } from 'src/shared/types/validators';
 import {
   HardwareSignTransaction,
   type SignHandle,
 } from 'src/ui/pages/HardwareWalletConnection/HardwareSignTransaction';
 import { walletPort } from 'src/ui/shared/channels';
-import { getWalletGroupByAddress } from 'src/ui/shared/requests/getWalletGroupByAddress';
 import { Button, type Kind as ButtonKind } from 'src/ui/ui-kit/Button';
-import type { HTMLDialogElementInterface } from 'src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface';
-import { ReadonlyWarningDialog } from './ReadonlyWarningDialog';
+import { WithReadonlyWarningDialog } from './ReadonlyWarningDialog';
 
 type SendTxParams = TransactionContextParams & {
   transaction: IncomingTransaction;
@@ -46,21 +41,6 @@ export const SignTransactionButton = React.forwardRef(
     ref: React.Ref<SignerSenderHandle>
   ) {
     const hardwareSignRef = useRef<SignHandle | null>(null);
-    const readonlyWarningDialogRef = useRef<HTMLDialogElementInterface | null>(
-      null
-    );
-    const { address } = wallet;
-    const { data: walletGroup } = useQuery({
-      queryKey: ['getWalletGroupByAddress', address],
-      queryFn: () => getWalletGroupByAddress(address),
-      // NOTE: if we use {suspense: true} here (default), SendTransaction view crashes
-      // with "max update depth exceeded" ¯\_(ツ)_/¯. Not a good sign, but afaik it should be fixed
-      // in the next version of '@tanstack/react-query'
-      suspense: false,
-    });
-    const isReadonlyGroup = walletGroup
-      ? isReadonlyContainer(walletGroup.walletContainer)
-      : null;
     const { mutateAsync: sendTransaction, ...sendTxMutation } = useMutation({
       mutationFn: async ({ transaction, ...params }: SendTxParams) => {
         if (isDeviceAccount(wallet)) {
@@ -98,27 +78,22 @@ export const SignTransactionButton = React.forwardRef(
         {...buttonProps}
       />
     ) : (
-      <>
-        <ReadonlyWarningDialog ref={readonlyWarningDialogRef} />
-        <Button
-          disabled={sendTxMutation.isLoading}
-          onClick={(event) => {
-            if (isReadonlyGroup) {
-              invariant(readonlyWarningDialogRef.current, 'Dialog not mounted');
-              event.preventDefault();
-              readonlyWarningDialogRef.current.showModal();
-            } else {
-              onClick?.(event);
-            }
-          }}
-          {...buttonProps}
-        >
-          {children ||
-            (sendTxMutation.isLoading
-              ? 'Sending...'
-              : buttonTitle || 'Confirm')}
-        </Button>
-      </>
+      <WithReadonlyWarningDialog
+        address={wallet.address}
+        onClick={onClick}
+        render={({ handleClick }) => (
+          <Button
+            disabled={sendTxMutation.isLoading}
+            onClick={handleClick}
+            {...buttonProps}
+          >
+            {children ||
+              (sendTxMutation.isLoading
+                ? 'Sending...'
+                : buttonTitle || 'Confirm')}
+          </Button>
+        )}
+      />
     );
   }
 );
