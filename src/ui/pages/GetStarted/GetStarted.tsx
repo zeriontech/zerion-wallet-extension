f743919627ee7a0192b92ef837299b9c680230e3
everdimension <everdimension@gmail.com>
2024-03-27 17:38:55 +0800

    Group add wallet options into a dialog

--- src/ui/pages/GetStarted/GetStarted.tsx
+++ src/ui/pages/GetStarted/GetStarted.tsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import { Link, Route, Routes, useSearchParams } from 'react-router-dom';
-import { Button } from 'src/ui/ui-kit/Button';
-import { PageTop } from 'src/ui/components/PageTop';
-import { PageColumn } from 'src/ui/components/PageColumn';
-import { Spacer } from 'src/ui/ui-kit/Spacer';
-import { VStack } from 'src/ui/ui-kit/VStack';
-import { UIText } from 'src/ui/ui-kit/UIText';
-import { SurfaceList } from 'src/ui/ui-kit/SurfaceList';
-import { FillView } from 'src/ui/components/FillView';
-import { AddressBadge } from 'src/ui/components/AddressBadge';
-import { useWalletGroups } from 'src/ui/shared/requests/useWalletGroups';
-import { PageBottom } from 'src/ui/components/PageBottom';
-import { getGroupDisplayName } from 'src/ui/shared/getGroupDisplayName';
-import type { WalletGroup } from 'src/shared/types/WalletGroup';
-import { NavigationTitle } from 'src/ui/components/NavigationTitle';
+import DownloadIcon from 'jsx:src/ui/assets/download.svg';
+import ConnectIcon from 'jsx:src/ui/assets/technology-connect.svg';
+import VisibleIcon from 'jsx:src/ui/assets/visible.svg';
 import ZerionSquircle from 'jsx:src/ui/assets/zerion-squircle.svg';
-import LedgerIcon from 'jsx:src/ui/assets/ledger-icon.svg';
-import { Background } from 'src/ui/components/Background';
-import { AngleRightRow } from 'src/ui/components/AngleRightRow';
+import type { WalletGroup } from 'src/shared/types/WalletGroup';
 import { isMnemonicContainer } from 'src/shared/types/validators';
-import { HStack } from 'src/ui/ui-kit/HStack';
+import { AddressBadge } from 'src/ui/components/AddressBadge';
+import { AngleRightRow } from 'src/ui/components/AngleRightRow';
+import { Background } from 'src/ui/components/Background';
+import { FillView } from 'src/ui/components/FillView';
+import { NavigationTitle } from 'src/ui/components/NavigationTitle';
+import { PageBottom } from 'src/ui/components/PageBottom';
+import { PageColumn } from 'src/ui/components/PageColumn';
+import { PageTop } from 'src/ui/components/PageTop';
+import { getGroupDisplayName } from 'src/ui/shared/getGroupDisplayName';
 import { openInTabView } from 'src/ui/shared/openInTabView';
-import { ImportWallet } from './ImportWallet';
-import { GenerateWallet } from './GenerateWallet';
+import { useWalletGroups } from 'src/ui/shared/requests/useWalletGroups';
+import { Button } from 'src/ui/ui-kit/Button';
+import { HStack } from 'src/ui/ui-kit/HStack';
+import { BottomSheetDialog } from 'src/ui/ui-kit/ModalDialogs/BottomSheetDialog';
+import { DialogTitle } from 'src/ui/ui-kit/ModalDialogs/DialogTitle';
+import type { HTMLDialogElementInterface } from 'src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface';
+import { Spacer } from 'src/ui/ui-kit/Spacer';
+import { SurfaceList } from 'src/ui/ui-kit/SurfaceList';
+import { UIText } from 'src/ui/ui-kit/UIText';
+import { VStack } from 'src/ui/ui-kit/VStack';
 import { AddReadonlyAddress } from './AddReadonlyAddress';
+import { GenerateWallet } from './GenerateWallet';
+import { ImportWallet } from './ImportWallet';
 
 function createNextHref(path: string, beforePath: string | null) {
   return beforePath ? `${beforePath}?next=${encodeURIComponent(path)}` : path;
@@ -74,6 +79,21 @@ function NewWalletOption({
   );
 }
 
+function SpaceZone({ children }: React.PropsWithChildren) {
+  return (
+    <div
+      style={{
+        borderRadius: 8,
+        padding: 6,
+        backgroundColor: 'var(--neutral-300)',
+        display: 'grid',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
 function Options() {
   const { data: walletGroups, isLoading } = useWalletGroups();
   const [params] = useSearchParams();
@@ -83,6 +103,10 @@ function Options() {
         isMnemonicContainer(group.walletContainer)
       ),
     [walletGroups]
+  );
+
+  const existingOptionsDialogRef = useRef<HTMLDialogElementInterface | null>(
+    null
   );
 
   if (isLoading) {
@@ -96,8 +120,69 @@ function Options() {
     '/connect-hardware-wallet',
     beforeCreate
   );
+  const addReadonlyHref = createNextHref('/get-started/readonly', beforeCreate);
   return (
     <Background backgroundKind="white">
+      <BottomSheetDialog
+        ref={existingOptionsDialogRef}
+        height="fit-content"
+        containerStyle={{ backgroundColor: 'var(--neutral-100)' }}
+        renderWhenOpen={() => (
+          <div style={{ textAlign: 'start' }}>
+            <UIText kind="headline/h3">
+              <DialogTitle
+                title="Add Existing Wallet"
+                alignTitle="start"
+                closeKind="icon"
+              />
+            </UIText>
+            <Spacer height={24} />
+            <SurfaceList
+              items={[
+                {
+                  key: 0,
+                  to: importHref,
+                  component: (
+                    <HStack gap={8} alignItems="center">
+                      <SpaceZone>
+                        <DownloadIcon />
+                      </SpaceZone>
+                      <div>
+                        <span>Import Wallet</span>
+                      </div>
+                    </HStack>
+                  ),
+                },
+                {
+                  key: 1,
+                  to: importLedgerHref,
+                  onClick: openInTabView,
+                  component: (
+                    <HStack gap={8} alignItems="center">
+                      <SpaceZone>
+                        <ConnectIcon />
+                      </SpaceZone>
+                      <span>Connect Hardware Wallet</span>
+                    </HStack>
+                  ),
+                },
+                {
+                  key: 2,
+                  to: addReadonlyHref,
+                  component: (
+                    <HStack gap={8} alignItems="center">
+                      <SpaceZone>
+                        <VisibleIcon />
+                      </SpaceZone>
+                      <span>Watch Address</span>
+                    </HStack>
+                  ),
+                },
+              ]}
+            />
+          </div>
+        )}
+      />
       <PageColumn>
         <NavigationTitle
           urlBar={isIntro ? 'none' : undefined}
@@ -121,23 +206,12 @@ function Options() {
             beforeCreate={beforeCreate}
             mnemonicWalletGroups={mnemonicGroups || null}
           />
-          <Button kind="regular" as={Link} to={importHref} size={44}>
-            Import Existing Wallet
-          </Button>
           <Button
             kind="regular"
-            as={Link}
-            to={importLedgerHref}
-            onClick={openInTabView}
             size={44}
+            onClick={() => existingOptionsDialogRef.current?.showModal()}
           >
-            <HStack gap={8} alignItems="center">
-              <LedgerIcon />
-              Connect Ledger
-            </HStack>
-          </Button>
-          <Button kind="regular" as={Link} to="/get-started/readonly" size={44}>
-            Watch Address
+            Add Existing Wallet
           </Button>
         </VStack>
         <PageBottom />
