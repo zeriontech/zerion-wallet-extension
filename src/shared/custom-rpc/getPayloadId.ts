7dbb85f1a0b5ba9dee0f9a2b734a0b706e291aa4
everdimension <everdimension@gmail.com>
2024-04-15 20:03:48 +0800

    Update payloadId()

--- src/shared/custom-rpc/getPayloadId.ts
+++ src/shared/custom-rpc/getPayloadId.ts
@@ -1,12 +1,9 @@
-let base = 1;
-const typedArray = new Uint16Array(1);
+let i = 1;
+const initialValue = crypto.getRandomValues(new Uint32Array(1))[0];
 
 /**
  * returns a unique random value always larger than the previous one
  */
 export function getPayloadId(): number {
-  if (base >= 2 ** 15 - 1) {
-    base = 1;
-  }
-  return (base++ << 16) + crypto.getRandomValues(typedArray)[0];
+  return initialValue + i++;
 }
