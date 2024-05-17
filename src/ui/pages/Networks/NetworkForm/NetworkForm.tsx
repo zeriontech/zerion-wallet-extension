17e3c57c51770d49af9aec46de52e927e917c3dc
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-05-17 17:29:45 +0300

    Allow longer network symbol names (#486)
    * Remove restrictions on network symbol length

--- src/ui/pages/Networks/NetworkForm/NetworkForm.tsx
+++ src/ui/pages/Networks/NetworkForm/NetworkForm.tsx
@@ -287,13 +287,7 @@ export function NetworkForm({
             label="Currency Symbol"
             name="nativeCurrency.symbol"
             defaultValue={chainConfig.nativeCurrency.symbol || ''}
-            pattern=".{2,8}"
             error={errors['nativeCurrency.symbol']}
-            onInvalid={(event) =>
-              event.currentTarget.setCustomValidity(
-                'At least 2 letters, maximum 8 letters'
-              )
-            }
             onInput={(event) => event.currentTarget.setCustomValidity('')}
             disabled={disabledFields?.has('nativeCurrency.symbol')}
             required
