d403af238b9cbb19cf6dfb9441f4beec8897c29c
everdimension <everdimension@gmail.com>
2024-05-22 18:26:04 +0300

    Fix name for SignMessageButton
    The rest diff is due to prettier and indentation ¯\_(ツ)_/¯

--- src/ui/components/SignMessageButton/SignMessageButton.tsx
+++ src/ui/components/SignMessageButton/SignMessageButton.tsx
@@ -24,101 +24,99 @@ export interface SignMsgBtnHandle {
   signTypedData_v4: (params: SignTypedDataParams) => Promise<string>;
 }
 
-export const SignMessageButton = React.forwardRef(
-  function SignTransactionButton(
-    {
-      wallet,
-      children,
-      buttonTitle,
-      buttonKind = 'primary',
-      onClick,
-      ...buttonProps
-    }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
-      wallet: ExternallyOwnedAccount;
-      buttonTitle?: React.ReactNode;
-      buttonKind?: ButtonKind;
-    },
-    ref: React.Ref<SignMsgBtnHandle>
-  ) {
-    const hardwareSignRef = useRef<SignMessageHandle | null>(null);
+export const SignMessageButton = React.forwardRef(function SignMessageButton(
+  {
+    wallet,
+    children,
+    buttonTitle,
+    buttonKind = 'primary',
+    onClick,
+    ...buttonProps
+  }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    wallet: ExternallyOwnedAccount;
+    buttonTitle?: React.ReactNode;
+    buttonKind?: ButtonKind;
+  },
+  ref: React.Ref<SignMsgBtnHandle>
+) {
+  const hardwareSignRef = useRef<SignMessageHandle | null>(null);
 
-    const { mutateAsync: personalSign, ...personalSignMutation } = useMutation({
-      mutationFn: async (params: PersonalSignParams) => {
+  const { mutateAsync: personalSign, ...personalSignMutation } = useMutation({
+    mutationFn: async (params: PersonalSignParams) => {
+      if (isDeviceAccount(wallet)) {
+        const { params: methodParams, ...messageContextParams } = params;
+        const [message] = methodParams;
+        invariant(
+          hardwareSignRef.current,
+          'HardwareSignMessage must be mounted'
+        );
+        const signature = await hardwareSignRef.current.personalSign(message);
+        walletPort.request('registerPersonalSign', {
+          message,
+          address: wallet.address,
+          ...messageContextParams,
+        });
+        return signature;
+      } else {
+        return await walletPort.request('personalSign', params);
+      }
+    },
+  });
+
+  const { mutateAsync: signTypedData_v4, ...signTypedData_v4Mutation } =
+    useMutation({
+      mutationFn: async (params: SignTypedDataParams) => {
         if (isDeviceAccount(wallet)) {
-          const { params: methodParams, ...messageContextParams } = params;
-          const [message] = methodParams;
+          const { typedData, ...messageContextParams } = params;
           invariant(
             hardwareSignRef.current,
             'HardwareSignMessage must be mounted'
           );
-          const signature = await hardwareSignRef.current.personalSign(message);
-          walletPort.request('registerPersonalSign', {
-            message,
+          const signature = await hardwareSignRef.current.signTypedData_v4(
+            typedData
+          );
+          walletPort.request('registerTypedDataSign', {
+            typedData,
             address: wallet.address,
             ...messageContextParams,
           });
           return signature;
         } else {
-          return await walletPort.request('personalSign', params);
+          return await walletPort.request('signTypedData_v4', params);
         }
       },
     });
 
-    const { mutateAsync: signTypedData_v4, ...signTypedData_v4Mutation } =
-      useMutation({
-        mutationFn: async (params: SignTypedDataParams) => {
-          if (isDeviceAccount(wallet)) {
-            const { typedData, ...messageContextParams } = params;
-            invariant(
-              hardwareSignRef.current,
-              'HardwareSignMessage must be mounted'
-            );
-            const signature = await hardwareSignRef.current.signTypedData_v4(
-              typedData
-            );
-            walletPort.request('registerTypedDataSign', {
-              typedData,
-              address: wallet.address,
-              ...messageContextParams,
-            });
-            return signature;
-          } else {
-            return await walletPort.request('signTypedData_v4', params);
-          }
-        },
-      });
+  useImperativeHandle(ref, () => ({ personalSign, signTypedData_v4 }));
 
-    useImperativeHandle(ref, () => ({ personalSign, signTypedData_v4 }));
+  const isLoading =
+    personalSignMutation.isLoading || signTypedData_v4Mutation.isLoading;
 
-    const isLoading =
-      personalSignMutation.isLoading || signTypedData_v4Mutation.isLoading;
-
-    return isDeviceAccount(wallet) ? (
-      <HardwareSignMessage
-        ref={hardwareSignRef}
-        derivationPath={wallet.derivationPath}
-        isSigning={isLoading}
-        children={children}
-        buttonTitle={buttonTitle}
-        buttonKind={buttonKind}
-        onClick={onClick}
-        {...buttonProps}
-      />
-    ) : (
-      <WithReadonlyWarningDialog
-        address={wallet.address}
-        onClick={onClick}
-        render={({ handleClick }) => (
-          <Button
-            disabled={isLoading}
-            onClick={handleClick}
-            kind={buttonKind}
-            {...buttonProps}
-          >
-            {children || (isLoading ? 'Signing...' : buttonTitle || 'Sign')}
-          </Button>
-        )}
-      />
-    );
-  }
-);
+  return isDeviceAccount(wallet) ? (
+    <HardwareSignMessage
+      ref={hardwareSignRef}
+      derivationPath={wallet.derivationPath}
+      isSigning={isLoading}
+      children={children}
+      buttonTitle={buttonTitle}
+      buttonKind={buttonKind}
+      onClick={onClick}
+      {...buttonProps}
+    />
+  ) : (
+    <WithReadonlyWarningDialog
+      address={wallet.address}
+      onClick={onClick}
+      render={({ handleClick }) => (
+        <Button
+          disabled={isLoading}
+          onClick={handleClick}
+          kind={buttonKind}
+          {...buttonProps}
+        >
+          {children || (isLoading ? 'Signing...' : buttonTitle || 'Sign')}
+        </Button>
+      )}
+    />
+  );
+});
