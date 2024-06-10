870e09f0184baab819dd3b0efee66b806f09bb2f
Andrey <zertsalov2910@gmail.com>
2024-06-10 14:00:45 +0100

    Redesign appearance page (#531)

--- src/ui/features/appearance/AppearancePage/AppearancePage.tsx
+++ src/ui/features/appearance/AppearancePage/AppearancePage.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { PageBottom } from 'src/ui/components/PageBottom';
 import { PageColumn } from 'src/ui/components/PageColumn';
 import { PageTop } from 'src/ui/components/PageTop';
-import { HStack } from 'src/ui/ui-kit/HStack';
-import { Radio } from 'src/ui/ui-kit/Radio';
-import { SurfaceList, SurfaceItemLabel } from 'src/ui/ui-kit/SurfaceList';
 import { UIText } from 'src/ui/ui-kit/UIText';
+import { useBackgroundKind } from 'src/ui/components/Background';
+import { Frame } from 'src/ui/ui-kit/Frame';
+import { FrameListItemButton } from 'src/ui/ui-kit/FrameList';
+import CheckIcon from 'jsx:src/ui/assets/check.svg';
+import { HStack } from 'src/ui/ui-kit/HStack';
 import { VStack } from 'src/ui/ui-kit/VStack';
 import { ThemePreference, preferenceStore } from '../preference-store';
 
@@ -16,51 +18,44 @@ const preferenceStrings = {
   [ThemePreference.system]: 'System',
 };
 
+const THEMES: ThemePreference[] = [
+  ThemePreference.light,
+  ThemePreference.system,
+  ThemePreference.dark,
+];
+
 export function AppearancePage() {
   const { mode } = useStore(preferenceStore);
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    preferenceStore.setState({
-      mode: Number(event.currentTarget.value),
-    });
-  };
+  useBackgroundKind({ kind: 'white' });
+
   return (
     <PageColumn>
       <PageTop />
-      <VStack gap={8}>
-        <SurfaceList
-          items={[
-            ThemePreference.light,
-            ThemePreference.system,
-            ThemePreference.dark,
-          ].map((preference, index) => ({
-            key: index,
-            isInteractive: true,
-            pad: false,
-            component: (
-              <SurfaceItemLabel>
-                <UIText
-                  kind="body/regular"
-                  color={mode === preference ? 'var(--primary)' : undefined}
-                >
-                  <HStack
-                    gap={8}
-                    alignItems="center"
-                    justifyContent="space-between"
-                  >
-                    <span>{preferenceStrings[preference]}</span>
-                    <Radio
-                      name="preference"
-                      value={preference}
-                      checked={mode === preference}
-                      onChange={handleChange}
-                    />
-                  </HStack>
+      <Frame>
+        <VStack gap={0}>
+          {THEMES.map((preference) => (
+            <FrameListItemButton
+              key={preference}
+              onClick={() => {
+                preferenceStore.setState({ mode: preference });
+              }}
+            >
+              <HStack
+                gap={8}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <UIText kind="body/accent">
+                  {preferenceStrings[preference]}
                 </UIText>
-              </SurfaceItemLabel>
-            ),
-          }))}
-        />
-      </VStack>
+                {mode === preference ? (
+                  <CheckIcon style={{ width: 24, height: 24 }} />
+                ) : null}
+              </HStack>
+            </FrameListItemButton>
+          ))}
+        </VStack>
+      </Frame>
       <PageBottom />
     </PageColumn>
   );
