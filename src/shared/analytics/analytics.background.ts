ffd013b8d654c5ac009486901604c5028960beed
Andrey <zertsalov2910@gmail.com>
2024-06-11 15:54:54 +0100

    Update src/shared/analytics/analytics.background.ts
    Co-authored-by: everdimension <everdimension@gmail.com>

--- src/shared/analytics/analytics.background.ts
+++ src/shared/analytics/analytics.background.ts
@@ -47,6 +47,7 @@ function trackAppEvents({ account }: { account: Account }) {
     const getUserId = () => account.getUser()?.id;
     return createBaseParams({ ...params, userId: getUserId() });
   };
+  /** {explicitly: true} means that user confirmed connection in a dialog. {false} means that we resolve a previously approved addess value */
   emitter.on('requestAccounts', ({ origin, address, explicitly }) => {
     if (!explicitly) {
       return;
