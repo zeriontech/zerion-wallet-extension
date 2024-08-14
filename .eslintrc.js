cecdef00bf9b47164d5141f6ac0119d6d957a554
everdimension <everdimension@gmail.com>
2024-08-14 15:31:44 +0300

    add rule to prevent unnecessary template literal quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   env: { browser: true },
   rules: {
     'no-console': 'warn',
+    quotes: ['error', 'single', { avoidEscape: true }],
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'no-use-before-define': ['error', { classes: false }],
