81a3ff113b4104f7bf2edaee4a0ef22d5fbf5692
everdimension <everdimension@gmail.com>
2024-08-21 17:13:01 +0300

    add comment

--- src/shared/sidepanel/sidepanel-messaging.ui.ts
+++ src/shared/sidepanel/sidepanel-messaging.ui.ts
@@ -1,6 +1,11 @@
 import browser from 'webextension-polyfill';
 import { urlContext } from '../UrlContext';
 
+/**
+ * TODO:
+ * Extract type for returned value to reuse
+ * between ui and background scripts
+ */
 export function initializeSidepanelEvents() {
   browser.runtime.onMessage.addListener(async (request) => {
     if (request.payload === 'is-sidepanel-open') {
