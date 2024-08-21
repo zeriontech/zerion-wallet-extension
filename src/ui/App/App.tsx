3b9c71567b6ce9d0d3faff8622fbd44ded763f4e
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-08-21 20:51:39 +0300

    Fix rebase

--- src/ui/App/App.tsx
+++ src/ui/App/App.tsx
@@ -195,210 +195,208 @@ function Views({ initialRoute }: { initialRoute?: string }) {
 
   const isPopup = urlContext.windowType === 'popup';
   return (
-    <RouteResolver>
-      <ViewArea>
-        <URLBar />
-        {isPopup ? <RouteRestoration initialRoute={initialRoute} /> : null}
-        <Routes>
-          {initialRoute ? (
-            <Route path="/" element={<Navigate to={initialRoute} />} />
-          ) : null}
-          <Route
-            path="/"
-            element={
-              <SomeKindOfResolver
-                noUser={<Navigate to="/intro" replace={true} />}
-                noWallet={<Navigate to="/get-started?intro" replace={true} />}
-                notAuthenticated={<Navigate to="/login" replace={true} />}
-                authenticated={<Navigate to="/overview" replace={true} />}
-              />
-            }
-          />
-          <Route path="/intro" element={<Intro />} />
-          <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/get-started/*" element={<GetStarted />} />
-          <Route
-            path="/connect-hardware-wallet/*"
-            element={<HardwareWalletConnection />}
-          />
-          <Route path="/receive" element={<Receive />} />
-          <Route path="/phishing-warning" element={<PhishingWarningPage />} />
-          <Route
-            path="/nft/:chain/:asset_code"
-            element={
+    <ViewArea>
+      <URLBar />
+      {isPopup ? <RouteRestoration initialRoute={initialRoute} /> : null}
+      <Routes>
+        {initialRoute ? (
+          <Route path="/" element={<Navigate to={initialRoute} />} />
+        ) : null}
+        <Route
+          path="/"
+          element={
+            <SomeKindOfResolver
+              noUser={<Navigate to="/intro" replace={true} />}
+              noWallet={<Navigate to="/get-started?intro" replace={true} />}
+              notAuthenticated={<Navigate to="/login" replace={true} />}
+              authenticated={<Navigate to="/overview" replace={true} />}
+            />
+          }
+        />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/get-started/*" element={<GetStarted />} />
+        <Route
+          path="/connect-hardware-wallet/*"
+          element={<HardwareWalletConnection />}
+        />
+        <Route path="/receive" element={<Receive />} />
+        <Route path="/phishing-warning" element={<PhishingWarningPage />} />
+        <Route
+          path="/nft/:chain/:asset_code"
+          element={
+            <RequireAuth>
+              <NonFungibleToken />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/overview/*"
+          element={
+            <RequireAuth>
+              <Overview />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/settings/*"
+          element={
+            <RequireAuth>
+              <Settings />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/networks-select"
+          element={
+            <RequireAuth>
+              <Networks />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/networks/*"
+          element={
+            <RequireAuth>
+              <Networks />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/reveal-private-key/*"
+          element={
+            <RequireAuth>
+              <RevealPrivateKey />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/requestAccounts"
+          element={
+            <ChooseGlobalProviderGuard>
               <RequireAuth>
                 <RequestAccounts />
               </RequireAuth>
-            }
-          />
-          <Route
-            path="/overview/*"
-            element={
-              <RequireAuth>
-                <Overview />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/settings/*"
-            element={
-              <RequireAuth>
-                <Settings />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/networks-select"
-            element={
-              <RequireAuth>
-                <Networks />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/networks/*"
-            element={
-              <RequireAuth>
-                <Networks />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/backup-wallet/*"
-            element={
-              <RequireAuth>
-                <BackupWallet />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/requestAccounts"
-            element={
-              <ChooseGlobalProviderGuard>
-                <RequireAuth>
-                  <RequestAccounts />
-                </RequireAuth>
-              </ChooseGlobalProviderGuard>
-            }
-          />
-          <Route
-            path="/sendTransaction/*"
-            element={
-              <RequireAuth>
-                <SendTransaction />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/siwe/*"
-            element={
-              <RequireAuth>
-                <SignInWithEthereum />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/signMessage"
-            element={
-              <RequireAuth>
-                <SignMessage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/signTypedData"
-            element={
-              <RequireAuth>
-                <SignTypedData />
-              </RequireAuth>
-            }
-          />
-          {/* TODO: Should this page be removed? */}
-          <Route
-            path="/switchEthereumChain"
-            element={
-              <RequireAuth>
-                <SwitchEthereumChain />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/addEthereumChain/*"
-            element={
-              <RequireAuth>
-                <AddEthereumChain />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/wallets/*"
-            element={
-              <RequireAuth>
-                <ManageWallets />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/wallet-select"
-            element={
-              <RequireAuth>
-                <WalletSelect />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/connected-sites/*"
-            element={
-              <RequireAuth>
-                <ConnectedSites />
-              </RequireAuth>
-            }
-          />
-          <Route path="/handshake-failure" element={<HandshakeFailure />} />
-          <Route
-            path="/ability/:ability_uid"
-            element={
-              <RequireAuth>
-                <AbilityPage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/send-form/*"
-            element={
-              <RequireAuth>
-                <SendForm />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/swap-form/*"
-            element={
-              <RequireAuth>
-                <SwapForm />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/not-implemented"
-            element={
-              <FillView>
-                <UIText
-                  kind="body/regular"
-                  color="var(--neutral-500)"
-                  style={{ padding: 20, textAlign: 'center' }}
-                >
-                  This View is not Implemented
-                </UIText>
-              </FillView>
-            }
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </ViewArea>
-    </RouteResolver>
+            </ChooseGlobalProviderGuard>
+          }
+        />
+        <Route
+          path="/sendTransaction/*"
+          element={
+            <RequireAuth>
+              <SendTransaction />
+            </RequireAuth>
+          }
+        />
+        <Route path="/testnetModeGuard" element={<TestnetModeGuard />} />
+        <Route
+          path="/siwe/*"
+          element={
+            <RequireAuth>
+              <SignInWithEthereum />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/signMessage"
+          element={
+            <RequireAuth>
+              <SignMessage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/signTypedData"
+          element={
+            <RequireAuth>
+              <SignTypedData />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/switchEthereumChain"
+          element={
+            <RequireAuth>
+              <SwitchEthereumChain />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/addEthereumChain/*"
+          element={
+            <RequireAuth>
+              <AddEthereumChain />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/wallets/*"
+          element={
+            <RequireAuth>
+              <ManageWallets />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/wallet-select"
+          element={
+            <RequireAuth>
+              <WalletSelect />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/connected-sites/*"
+          element={
+            <RequireAuth>
+              <ConnectedSites />
+            </RequireAuth>
+          }
+        />
+        <Route path="/handshake-failure" element={<HandshakeFailure />} />
+        <Route
+          path="/ability/:ability_uid"
+          element={
+            <RequireAuth>
+              <AbilityPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/send-form/*"
+          element={
+            <RequireAuth>
+              <SendForm />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/swap-form/*"
+          element={
+            <RequireAuth>
+              <SwapForm />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/not-implemented"
+          element={
+            <FillView>
+              <UIText
+                kind="body/regular"
+                color="var(--neutral-500)"
+                style={{ padding: 20, textAlign: 'center' }}
+              >
+                This View is not Implemented
+              </UIText>
+            </FillView>
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </ViewArea>
   );
 }
 
