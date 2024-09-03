19b344d315d93d3eb08febce69ddad9d2b2e51c9
everdimension <everdimension@gmail.com>
2024-09-03 22:03:13 +0300

    Update sidepanel flow

--- src/ui/shared/routing/ProgrammaticNavigationHelper/ProgrammaticNavigationHelper.tsx
+++ src/ui/shared/routing/ProgrammaticNavigationHelper/ProgrammaticNavigationHelper.tsx
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { emitter } from '../../events';
 
+/**
+ * TODO: migrate to {const router = createBrowserRouter(...) + <RouterProvider router={router} />}:
+ * https://reactrouter.com/en/main/upgrading/v6-data
+ */
 export function ProgrammaticNavigationHelper() {
   const navigate = useNavigate();
   useEffect(() => {
