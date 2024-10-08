b7a651844e8104fd389a657bb85811acbec0cdf4
everdimension <everdimension@gmail.com>
2024-10-08 16:23:13 +0300

    Fix types for wallet-get-meta (#624)
    The types in defi-sdk use snake_case and types
    in http-api use camelCase

--- src/modules/zerion-api/requests/wallet-get-meta.ts
+++ src/modules/zerion-api/requests/wallet-get-meta.ts
@@ -1,4 +1,3 @@
-import type { AddressMembership } from 'defi-sdk';
 import type { ClientOptions } from '../shared';
 import { CLIENT_DEFAULTS, ZerionHttpClient } from '../shared';
 
@@ -6,6 +5,91 @@ export interface Identity {
   provider: 'ens' | 'lens' | 'ud' | 'unspecified';
   address: string;
   handle: string;
+}
+
+interface MigrationToken {
+  generation: 'G1' | 'OnePointO';
+  id: string;
+  premium: {
+    expirationTime: string | null;
+    plan: 'Single' | 'Bundle';
+    features: {
+      feeWaiver: boolean;
+      csv: boolean;
+      pnl: boolean;
+      perks: boolean;
+      earlyAccess: boolean;
+    };
+    bundle:
+      | {
+          address: string;
+          updateAllowedAt: string;
+        }[]
+      | null;
+  };
+}
+
+export declare type ParentToken = MigrationToken & {
+  owner: string;
+};
+
+export interface XpDistribution {
+  earned: number;
+  locked: number;
+  referred: number;
+}
+
+interface ReferrerNFT {
+  chain: string;
+  contractAddress: string;
+  tokenId: string;
+  metadata: {
+    name: string | null;
+    content: {
+      type: string;
+      audioUrl: string | null;
+      imagePreviewUrl: string | null;
+      imageUrl: string | null;
+      videoUrl: string | null;
+    } | null;
+  };
+}
+
+interface Referrer {
+  referralCode: string;
+  address: string | null;
+  handle: string | null;
+  nft: ReferrerNFT | null;
+}
+
+interface NonPremiumTokens {
+  id: string;
+  generation: 'G1' | 'OnePointO';
+}
+
+interface MigrationBalances {
+  id: string;
+  initial: number;
+  remained: number;
+}
+
+interface AddressMembership {
+  level: number;
+  levelProgress: number;
+  premium: MigrationToken['premium'] | null;
+  migration: {
+    balances: MigrationBalances[];
+    nonPremiumTokens: NonPremiumTokens[];
+    migrationEndTime: string;
+    trialEndTime: string;
+  } | null;
+  referralCode: string | null;
+  referralLink: string | null;
+  referred: number;
+  referrer: Referrer;
+  tokens: MigrationToken[] | null;
+  parentTokens: ParentToken[] | null;
+  xp: XpDistribution;
 }
 
 export interface WalletMeta {
