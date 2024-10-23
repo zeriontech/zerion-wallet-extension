9dd287ddf925bb5d58c811dcb7d344bd3b19836e
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-10-23 03:53:20 +0300

    Renaming

--- src/ui/features/referral-program/shared/storage.ts
+++ src/ui/features/referral-program/shared/storage.ts
@@ -3,10 +3,10 @@ import * as browserStorage from 'src/background/webapis/storage';
 
 const referrerKey = 'referral-program/referrer-2024-10-10';
 
-export async function readReferrer() {
+export async function readSavedReferrerData() {
   return await browserStorage.get<ReferrerData>(referrerKey);
 }
 
-export async function saveReferrer(referrer: ReferrerData) {
-  await browserStorage.set(referrerKey, referrer);
+export async function saveReferrerData(referrerData: ReferrerData) {
+  await browserStorage.set(referrerKey, referrerData);
 }
