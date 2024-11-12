d9c8630ac84a7ae1e4ce1b1fa40ff0d33869f543
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-11-12 14:37:44 +0700

    Revert unnecessary diff

--- src/ui/components/AddressBadge/AddressBadge.tsx
+++ src/ui/components/AddressBadge/AddressBadge.tsx
@@ -27,10 +27,7 @@ export function AddressBadge({
       <WalletAvatar address={wallet.address} size={14} borderRadius={4} />
       <UIText
         kind="caption/regular"
-        style={{
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-        }}
+        style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
       >
         <WalletDisplayName wallet={wallet} maxCharacters={16} padding={4} />
       </UIText>
