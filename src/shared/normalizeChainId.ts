3ba1a82a44be7fab2f6b29aa023440b66159c5aa
Andrey <zertsalov2910@gmail.com>
2025-01-14 12:36:25 +0000

    Lowercase chainId when normalizing (#693)

--- src/shared/normalizeChainId.ts
+++ src/shared/normalizeChainId.ts
@@ -5,5 +5,5 @@ import { valueToHex } from './units/valueToHex';
 export function normalizeChainId(
   value: string | number | bigint | BigNumber
 ): ChainId {
-  return valueToHex(value) as ChainId;
+  return valueToHex(value).toLowerCase() as ChainId;
 }
