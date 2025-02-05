b67a7c6fe804edad3a87cec90e4c1946a9668d20
everdimension <everdimension@gmail.com>
2025-02-05 20:39:49 +0300

    [wip]

--- src/shared/wallet/derivation-paths.ts
+++ src/shared/wallet/derivation-paths.ts
@@ -2,7 +2,7 @@ const next = (index: number) => `m/44'/60'/0'/0/${index}`;
 
 export const getAccountPath = next;
 
-export type DerivationPathType = 'bip44' | 'ledger' | 'ledgerLive';
+export type DerivationPathType = 'bip44' | 'ledger' | 'ledgerLive' | 'solana';
 
 // As per https://github.com/ethereum/EIPs/issues/84#issuecomment-517505282:
 // m/44'/60'/0'/0/x: BIP44, MetaMask, Jaxx, MyEtherWallet (default), TREZOR App, Exodus
@@ -13,7 +13,16 @@ const pathPatterns: Record<DerivationPathType, RegExp> = {
   bip44: /(?:m\/)?44'\/60'\/0'\/0\/(\d+)/,
   ledgerLive: /(?:m\/)?44'\/60'\/(\d+)'\/0\/0/,
   ledger: /(?:m\/)?44'\/60'\/0'\/(\d+)/,
+  solana: /(?:m\/)?44'\/501'\/0'\/(\d+)'/,
 };
+
+/** This isn't the most elegant, but we can determine solana addresses based on derivation path */
+export function isSolanaPath(path: string) {
+  return pathPatterns.solana.test(path);
+}
+
+export const getAccountPathSolana = (index: number) =>
+  `m/44'/501'/0'/${index}'`;
 
 export function getIndexFromPath(
   path: string,
@@ -22,4 +31,14 @@ export function getIndexFromPath(
   const pattern = pathPatterns[type];
   const digits = path.match(pattern)?.[1];
   return digits ? Number(digits) : 0;
+}
+
+export function inferIndexFromDerivationPath(path: string) {
+  for (const pattern of Object.values(pathPatterns)) {
+    const match = path.match(pattern);
+    if (match) {
+      return Number(match[1]);
+    }
+  }
+  return 0;
 }
--- src/ui/pages/GetStarted/ImportWallet/MnemonicImportView/getFirstNMnemonicWallets.worker.ts
+++ src/ui/pages/GetStarted/ImportWallet/MnemonicImportView/getFirstNMnemonicWallets.worker.ts
@@ -1,35 +1,50 @@
 import { HDNode } from '@ethersproject/hdnode';
-import type { MaskedBareWallet } from 'src/shared/types/BareWallet';
-import { getAccountPath } from 'src/shared/wallet/derivation-paths';
+import { Keypair } from '@solana/web3.js';
+import * as bip39 from 'bip39';
+import { derivePath } from 'ed25519-hd-key';
+import type { BareWallet, MaskedBareWallet } from 'src/shared/types/BareWallet';
+import {
+  getAccountPath,
+  getAccountPathSolana,
+} from 'src/shared/wallet/derivation-paths';
 import type { LocallyEncoded } from 'src/shared/wallet/encode-locally';
 import {
   decodeMasked,
   encodeForMasking,
 } from 'src/shared/wallet/encode-locally';
+import { fromSolanaKeypair } from 'src/shared/wallet/solana';
 
 export interface Params {
+  /**
+   * 'ecdsa' is used to create ethereum key pairs;
+   * 'ed25519' is used to create solana key pairs;
+   */
+  curve: 'ecdsa' | 'ed25519';
   phrase: LocallyEncoded;
   n: number;
 }
 
 export type Result = MaskedBareWallet[];
 
+function locallyMaskWallet(wallet: BareWallet | HDNode) {
+  return {
+    mnemonic: wallet.mnemonic
+      ? { ...wallet.mnemonic, phrase: encodeForMasking(wallet.mnemonic.phrase) }
+      : null,
+    privateKey: encodeForMasking(wallet.privateKey),
+    address: wallet.address,
+    name: 'name' in wallet ? wallet.name ?? null : null,
+  };
+}
+
 function fromHDNode(hdNode: HDNode): MaskedBareWallet {
   if (!hdNode.mnemonic) {
     throw new Error('Expected an HDNode with a mnemonic');
   }
-  return {
-    mnemonic: {
-      ...hdNode.mnemonic,
-      phrase: encodeForMasking(hdNode.mnemonic.phrase),
-    },
-    privateKey: encodeForMasking(hdNode.privateKey),
-    address: hdNode.address,
-    name: null,
-  };
+  return locallyMaskWallet(hdNode);
 }
 
-function getFirstNMnemonicWallets({ phrase, n }: Params) {
+function getFirstNMnemonicWalletsEcdsa({ phrase, n }: Omit<Params, 'curve'>) {
   const result: Result = [];
   // NOTE:
   // ethers.utils.HDNode is _much_ faster at generating wallets
@@ -43,8 +58,36 @@ function getFirstNMnemonicWallets({ phrase, n }: Params) {
   return result;
 }
 
+/** Solana keypairs */
+function getFirstNMnemonicWalletsEd25519({
+  phrase: phraseEncoded,
+  n,
+}: Omit<Params, 'curve'>) {
+  const result: Result = [];
+  const phrase = decodeMasked(phraseEncoded);
+  const seed = bip39.mnemonicToSeedSync(phrase);
+  for (let i = 0; i < n; i++) {
+    const path = getAccountPathSolana(i);
+    const { key: derivedSeed } = derivePath(path, seed.toString('hex'));
+    const keypair = Keypair.fromSeed(derivedSeed);
+    const wallet = fromSolanaKeypair({ mnemonic: { phrase, path }, keypair });
+    result.push(locallyMaskWallet(wallet));
+  }
+  return result;
+}
+
+function main(params: Params) {
+  if (params.curve === 'ed25519') {
+    // TODO: measure performance time
+    return getFirstNMnemonicWalletsEd25519(params);
+  } else if (params.curve === 'ecdsa') {
+    return getFirstNMnemonicWalletsEcdsa(params);
+  } else {
+    throw new Error(`Must pass known curve type: ${params.curve}`);
+  }
+}
+
 global.onmessage = (event: MessageEvent<Params>) => {
-  const { phrase, n } = event.data;
-  const result = getFirstNMnemonicWallets({ phrase, n });
+  const result = main(event.data);
   global.postMessage(result);
 };
