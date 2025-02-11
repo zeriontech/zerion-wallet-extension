8cf4c914a9f7427bb7bef2e53371db69243a23a8
everdimension <everdimension@gmail.com>
2025-02-11 22:53:53 +0300

    Implement disconnect method

--- src/background/Wallet/Wallet.ts
+++ src/background/Wallet/Wallet.ts
@@ -558,6 +558,17 @@ export class Wallet {
     this.emitter.emit('permissionsUpdated');
   }
 
+  async removeSolanaPermissions({
+    context,
+    params: { origin },
+  }: WalletMethodParams<{ origin: string; address?: string }>) {
+    this.verifyInternalOrigin(context);
+    this.ensureRecord(this.record);
+    this.record = Model.removeSolanaPermissions(this.record, { origin });
+    this.updateWalletStore(this.record);
+    this.emitter.emit('permissionsUpdated');
+  }
+
   async removePermission({
     context,
     params: { origin, address },
@@ -1840,6 +1851,15 @@ class PublicController {
       { id, context, params: [] },
       { ecosystem: 'solana' }
     );
+  }
+
+  async sol_disconnect({ context }: PublicMethodParams) {
+    invariant(context?.origin, 'This method requires origin');
+
+    return this.wallet.removeSolanaPermissions({
+      context: INTERNAL_SYMBOL_CONTEXT,
+      params: { origin: context.origin },
+    });
   }
 
   async sol_signTransaction({
