f935966c4df211be3f2a405d76a4b69b9289eb1e
everdimension <everdimension@gmail.com>
2025-02-11 18:05:36 +0300

    Support signTransaction method

--- src/modules/solana/transactions/create.ts
+++ src/modules/solana/transactions/create.ts
@@ -1,18 +1,25 @@
-import type { VersionedTransaction } from '@solana/web3.js';
+import { VersionedTransaction, Transaction } from '@solana/web3.js';
 import {
   base64ToUint8Array,
   uint8ArrayToBase64,
 } from 'src/modules/crypto/convert';
-import { SolTransaction } from 'src/modules/solana/SolTransaction';
+import type { SolTransaction } from 'src/modules/solana/SolTransaction';
 
 export function solFromBase64(base64: string): SolTransaction {
   const typedArray = base64ToUint8Array(base64);
-  return SolTransaction.from(typedArray);
+  try {
+    return Transaction.from(typedArray);
+  } catch {
+    return VersionedTransaction.deserialize(typedArray);
+  }
 }
 
 export function solToBase64(
-  transaction: SolTransaction | VersionedTransaction
+  transaction: SolTransaction | VersionedTransaction,
+  { requireAllSignatures } = { requireAllSignatures: false }
 ): string {
-  const serialized = transaction.serialize({ requireAllSignatures: false });
+  const serialized = transaction.serialize({ requireAllSignatures });
   return uint8ArrayToBase64(serialized);
 }
+
+Object.assign(globalThis, { solFromBase64, solToBase64 });
