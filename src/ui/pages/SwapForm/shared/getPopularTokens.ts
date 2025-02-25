3d095dfc70b899d345f109b3d75613f0598bf997
Andrey <zertsalov96@yandex.ru>
2025-02-25 15:18:12 +0000

    fix comments

--- src/ui/pages/SwapForm/shared/getPopularTokens.ts
+++ src/ui/pages/SwapForm/shared/getPopularTokens.ts
@@ -1,21 +1,10 @@
 import ky from 'ky';
 import { DEFI_SDK_TRANSACTIONS_API_URL } from 'src/env/config';
 import type { Chain } from 'src/modules/networks/Chain';
-import { invariant } from 'src/shared/invariant';
 
 export async function getPopularTokens(chain: Chain) {
-  const params = new URLSearchParams({ chain: chain.toString() });
-  invariant(
-    DEFI_SDK_TRANSACTIONS_API_URL,
-    'DEFI_SDK_TRANSACTIONS_API_URL must be defined in ENV'
-  );
-  const result = await ky
-    .get(
-      new URL(
-        `/swap/popularTokens?${params.toString()}`,
-        DEFI_SDK_TRANSACTIONS_API_URL
-      )
-    )
-    .json<{ assets: string[] }>();
+  const url = new URL('/swap/popularTokens', DEFI_SDK_TRANSACTIONS_API_URL);
+  url.searchParams.append('chain', chain.toString());
+  const result = await ky.get(url).json<{ assets: string[] }>();
   return result.assets;
 }
