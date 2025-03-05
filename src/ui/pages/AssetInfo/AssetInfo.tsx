27c80d6c635cb54b7eea69f48818bfab43c51b18
Andrey <zertsalov96@yandex.ru>
2025-03-05 15:37:55 +0000

    add implementations

--- src/ui/pages/AssetInfo/AssetInfo.tsx
+++ src/ui/pages/AssetInfo/AssetInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useCurrency } from 'src/modules/currency/useCurrency';
 import { invariant } from 'src/shared/invariant';
@@ -34,6 +34,8 @@ import { useNetworks } from 'src/modules/networks/useNetworks';
 import DownIcon from 'jsx:src/ui/assets/chevron-down.svg';
 import CopyIcon from 'jsx:src/ui/assets/copy.svg';
 import CheckIcon from 'jsx:src/ui/assets/check_double.svg';
+import LinkIcon from 'jsx:src/ui/assets/new-window.svg';
+import ArrowLeftIcon from 'jsx:src/ui/assets/arrow-left.svg';
 import { useCopyToClipboard } from 'src/ui/shared/useCopyToClipboard';
 import type { NetworkConfig } from 'src/modules/networks/NetworkConfig';
 import { middleTruncate } from 'src/ui/shared/middleTruncate';
@@ -52,6 +54,9 @@ import { NeutralDecimals } from 'src/ui/ui-kit/NeutralDecimals';
 import { useWalletAssetDetails } from 'src/modules/zerion-api/hooks/useWalletAssetDetails';
 import type { ExternallyOwnedAccount } from 'src/shared/types/ExternallyOwnedAccount';
 import type { WalletAssetDetails } from 'src/modules/zerion-api/requests/wallet-get-asset-details';
+import type { HTMLDialogElementInterface } from 'src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface';
+import { CenteredDialog } from 'src/ui/ui-kit/ModalDialogs/CenteredDialog';
+import { UnstyledButton } from 'src/ui/ui-kit/UnstyledButton';
 import * as styles from './styles.module.css';
 import { AssetHistory } from './AssetHistory';
 
@@ -446,13 +451,54 @@ function AssetImplementationButton({
   const { handleCopy, isSuccess } = useCopyToClipboard({
     text: address || 'null',
   });
+
+  if (!address) {
+    return (
+      <Button
+        as={UnstyledAnchor}
+        rel="noopener noreferrer"
+        target="_blank"
+        href={network.explorer_home_url || ''}
+        aria-label="Copy token address"
+        size={36}
+        kind="neutral"
+        className="parent-hover"
+        style={{
+          ['--parent-content-color' as string]: 'var(--neutral-400)',
+          ['--parent-hovered-content-color' as string]: 'var(--neutral-700)',
+          ['--button-background' as string]: 'var(--neutral-200)',
+          ['--button-background-hover' as string]: 'var(--neutral-300)',
+          paddingInline: 8,
+        }}
+      >
+        <HStack gap={4} alignItems="center">
+          <HStack gap={6} alignItems="center">
+            <img
+              src={network.icon_url}
+              alt={network.name}
+              width={24}
+              height={24}
+            />
+            <UIText kind="small/accent">Explorer</UIText>
+          </HStack>
+          <LinkIcon
+            className="content-hover"
+            style={{ width: 20, height: 20 }}
+          />
+        </HStack>
+      </Button>
+    );
+  }
   return (
     <Button
       onClick={handleCopy}
       aria-label="Copy token address"
       size={36}
       kind="neutral"
+      className="parent-hover"
       style={{
+        ['--parent-content-color' as string]: 'var(--neutral-400)',
+        ['--parent-hovered-content-color' as string]: 'var(--neutral-700)',
         ['--button-background' as string]: 'var(--neutral-200)',
         ['--button-background-hover' as string]: 'var(--neutral-300)',
       }}
@@ -466,7 +512,7 @@ function AssetImplementationButton({
             height={24}
           />
           <UIText kind="small/accent">
-            {address ? middleTruncate({ value: address }) : 'Native'}
+            {middleTruncate({ value: address })}
           </UIText>
         </HStack>
         {isSuccess ? (
@@ -475,7 +521,8 @@ function AssetImplementationButton({
           />
         ) : (
           <CopyIcon
-            style={{ color: 'var(--neutral-500)', width: 20, height: 20 }}
+            className="content-hover"
+            style={{ width: 20, height: 20 }}
           />
         )}
       </HStack>
@@ -483,8 +530,96 @@ function AssetImplementationButton({
   );
 }
 
+function CopyAddressButton({ address }: { address: string }) {
+  const { handleCopy, isSuccess } = useCopyToClipboard({ text: address });
+
+  return (
+    <UnstyledButton
+      onClick={handleCopy}
+      aria-label="Copy token address"
+      className="parent-hover"
+      style={{
+        ['--parent-content-color' as string]: 'var(--neutral-400)',
+        ['--parent-hovered-content-color' as string]: 'var(--neutral-700)',
+      }}
+    >
+      {isSuccess ? <CheckIcon /> : <CopyIcon className="content-hover" />}
+    </UnstyledButton>
+  );
+}
+
+function AssetImplementationsDialog({
+  implementations,
+}: {
+  implementations: { address: string | null; network: NetworkConfig }[];
+}) {
+  const { networks } = useNetworks();
+  return (
+    <VStack
+      gap={14}
+      style={{ ['--surface-background-color' as string]: 'var(--white)' }}
+    >
+      <UIText kind="body/accent" style={{ justifySelf: 'center' }}>
+        Explorers
+      </UIText>
+      <VStack gap={0}>
+        {implementations.map(({ address, network }) => (
+          <HStack
+            key={network.id}
+            gap={24}
+            justifyContent="space-between"
+            alignItems="center"
+            style={{ paddingBlock: 12 }}
+          >
+            <HStack gap={12} alignItems="center">
+              <img
+                src={network.icon_url}
+                alt={network.name}
+                width={36}
+                height={36}
+              />
+              <VStack gap={0}>
+                <UIText kind="body/accent">{network.name}</UIText>
+                {address ? (
+                  <UIText kind="small/regular" color="var(--neutral-500)">
+                    {middleTruncate({ value: address })}
+                  </UIText>
+                ) : null}
+              </VStack>
+            </HStack>
+            <HStack gap={0}>
+              {address ? <CopyAddressButton address={address} /> : null}
+              <UnstyledAnchor
+                rel="noopener noreferrer"
+                target="_blank"
+                href={
+                  address && networks
+                    ? networks.getExplorerTokenUrlByName(
+                        createChain(network.id),
+                        address
+                      )
+                    : network.explorer_home_url || ''
+                }
+                className="parent-hover"
+                style={{
+                  ['--parent-content-color' as string]: 'var(--neutral-400)',
+                  ['--parent-hovered-content-color' as string]:
+                    'var(--neutral-700)',
+                }}
+              >
+                <LinkIcon className="content-hover" />
+              </UnstyledAnchor>
+            </HStack>
+          </HStack>
+        ))}
+      </VStack>
+    </VStack>
+  );
+}
+
 function AssetResources({ assetFullInfo }: { assetFullInfo: AssetFullInfo }) {
   const { networks } = useNetworks();
+  const dialogRef = useRef<HTMLDialogElementInterface | null>(null);
   const implementations = useMemo(
     () =>
       Object.entries(assetFullInfo.fungible.implementations)
@@ -497,46 +632,73 @@ function AssetResources({ assetFullInfo }: { assetFullInfo: AssetFullInfo }) {
   );
 
   return (
-    <HStack gap={24} justifyContent="space-between">
-      <HStack gap={8}>
-        {assetFullInfo.extra.relevantResources.map((resource) => (
-          <Button
-            key={resource.name}
-            as={UnstyledAnchor}
-            rel="noopenner noreferrer"
-            target="_blank"
-            kind="neutral"
-            size={36}
-            href={resource.url}
-            style={{ padding: 6, border: '2px solid var(--neutral-200)' }}
-            aria-label={resource.displayableName}
-          >
-            <img src={resource.iconUrl} width={20} height={20} />
-          </Button>
-        ))}
+    <>
+      <HStack gap={24} justifyContent="space-between">
+        <HStack gap={8}>
+          {assetFullInfo.extra.relevantResources.map((resource) => (
+            <Button
+              key={resource.name}
+              as={UnstyledAnchor}
+              rel="noopenner noreferrer"
+              target="_blank"
+              kind="neutral"
+              size={36}
+              href={resource.url}
+              style={{ padding: 6, border: '2px solid var(--neutral-200)' }}
+              aria-label={resource.displayableName}
+            >
+              <img src={resource.iconUrl} width={20} height={20} />
+            </Button>
+          ))}
+        </HStack>
+        <HStack gap={4}>
+          {implementations.length ? (
+            <AssetImplementationButton
+              address={implementations[0].address}
+              network={implementations[0].network}
+            />
+          ) : null}
+          {implementations.length > 1 ? (
+            <Button
+              kind="regular"
+              size={36}
+              onClick={() => dialogRef.current?.showModal()}
+              style={{
+                padding: 8,
+                ['--button-background' as string]: 'var(--neutral-200)',
+                ['--button-background-hover' as string]: 'var(--neutral-300)',
+              }}
+            >
+              <DownIcon style={{ width: 20, height: 20 }} />
+            </Button>
+          ) : null}
+        </HStack>
       </HStack>
-      <HStack gap={4}>
-        {implementations.length ? (
-          <AssetImplementationButton
-            address={implementations[0].address}
-            network={implementations[0].network}
-          />
-        ) : null}
-        {implementations.length > 1 ? (
-          <Button
-            kind="regular"
-            size={36}
-            style={{
-              padding: 8,
-              ['--button-background' as string]: 'var(--neutral-200)',
-              ['--button-background-hover' as string]: 'var(--neutral-300)',
-            }}
-          >
-            <DownIcon style={{ width: 20, height: 20 }} />
-          </Button>
-        ) : null}
-      </HStack>
-    </HStack>
+      <CenteredDialog
+        ref={dialogRef}
+        containerStyle={{ backgroundColor: 'var(--neutral-100)' }}
+        renderWhenOpen={() => (
+          <>
+            <Button
+              kind="ghost"
+              value="cancel"
+              size={40}
+              style={{
+                width: 40,
+                padding: 8,
+                position: 'absolute',
+                top: 16,
+                left: 8,
+              }}
+              onClick={() => dialogRef.current?.close()}
+            >
+              <ArrowLeftIcon />
+            </Button>
+            <AssetImplementationsDialog implementations={implementations} />
+          </>
+        )}
+      />
+    </>
   );
 }
 
