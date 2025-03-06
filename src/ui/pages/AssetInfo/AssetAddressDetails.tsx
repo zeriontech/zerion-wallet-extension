fb1fbcbc044df435fa2a232b4c4a367c83dd3deb
Andrey <zertsalov96@yandex.ru>
2025-03-06 12:01:56 +0000

    add assets address details

--- src/ui/pages/AssetInfo/AssetAddressDetails.tsx
+++ src/ui/pages/AssetInfo/AssetAddressDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HStack } from 'src/ui/ui-kit/HStack';
 import { TokenIcon } from 'src/ui/ui-kit/TokenIcon';
 import { UIText } from 'src/ui/ui-kit/UIText';
@@ -8,7 +8,7 @@ import {
   formatCurrencyToParts,
   formatCurrencyValue,
 } from 'src/shared/units/formatCurrencyValue';
-import { emDash, middot } from 'src/ui/shared/typography';
+import { emDash, middot, minus } from 'src/ui/shared/typography';
 import { useCurrency } from 'src/modules/currency/useCurrency';
 import WalletIcon from 'jsx:src/ui/assets/wallet-fancy.svg';
 import type {
@@ -21,6 +21,10 @@ import { UnstyledAnchor } from 'src/ui/ui-kit/UnstyledAnchor';
 import { formatPercent } from 'src/shared/units/formatPercent/formatPercent';
 import { formatTokenValue } from 'src/shared/units/formatTokenValue';
 import { NeutralDecimals } from 'src/ui/ui-kit/NeutralDecimals';
+import { useWalletPortfolio } from 'src/modules/zerion-api/hooks/useWalletPortfolio';
+import { useHttpClientSource } from 'src/modules/zerion-api/hooks/useHttpClientSource';
+import { UnstyledButton } from 'src/ui/ui-kit/UnstyledButton';
+import ArrowDownIcon from 'jsx:src/ui/assets/caret-down-filled.svg';
 
 export function AssetHeader({
   asset,
@@ -65,14 +69,66 @@ function Line() {
 
 const DEFAULT_APP_ID = 'wallet';
 
+function StatLine({
+  title,
+  value,
+  valueColor,
+}: {
+  title: string;
+  value: string;
+  valueColor?: string;
+}) {
+  return (
+    <HStack gap={16} justifyContent="space-between" alignItems="center">
+      <UIText kind="body/regular">{title}</UIText>
+      <UIText kind="body/accent" color={valueColor}>
+        {value}
+      </UIText>
+    </HStack>
+  );
+}
+
+function getColor(value?: number) {
+  return !value
+    ? 'var(--black)'
+    : value > 0
+    ? 'var(--positive-500)'
+    : 'var(--negative-500)';
+}
+
+function getSign(value?: number) {
+  return !value ? '' : value > 0 ? '+' : minus;
+}
+
 export function AssetAddressDetails({
+  address,
   assetFullInfo,
   walletAssetDetails,
 }: {
+  address: string;
   assetFullInfo: AssetFullInfo;
   walletAssetDetails: WalletAssetDetails;
 }) {
   const { currency } = useCurrency();
+  const [showNetworkDistribution, setShowNetworkDistribution] = useState(false);
+
+  const { data: portfolioData } = useWalletPortfolio(
+    {
+      addresses: [address],
+      currency,
+      nftPriceType: 'not_included',
+    },
+    { source: useHttpClientSource() }
+  );
+
+  const totalEquityPercentage = portfolioData?.data.totalValue
+    ? (walletAssetDetails.totalValue / portfolioData.data.totalValue) * 100
+    : null;
+
+  const return24h =
+    assetFullInfo.fungible.meta.relativeChange1d *
+    assetFullInfo.fungible.meta.price;
+  const relativeReturn24h = assetFullInfo.fungible.meta.relativeChange1d;
 
   return (
     <VStack
@@ -87,6 +143,18 @@ export function AssetAddressDetails({
         <VStack gap={12}>
           <HStack gap={8} alignItems="center">
             <UIText kind="small/accent">Equity</UIText>
+            {totalEquityPercentage ? (
+              <UIText
+                kind="caption/accent"
+                style={{
+                  padding: '4px 6px',
+                  backgroundColor: 'var(--neutral-200)',
+                  borderRadius: 8,
+                }}
+              >
+                {formatPercent(totalEquityPercentage, 'en')}%
+              </UIText>
+            ) : null}
           </HStack>
           <VStack gap={4}>
             <UIText kind="headline/h1">
@@ -104,6 +172,157 @@ export function AssetAddressDetails({
                 assetFullInfo.fungible.symbol
               )}
             </UIText>
+          </VStack>
+        </VStack>
+        <VStack gap={16}>
+          <StatLine
+            title="24-hour Return"
+            value={`${getSign(return24h)}${formatCurrencyValue(
+              Math.abs(return24h || 0),
+              'en',
+              currency
+            )} (${formatPercent(Math.abs(relativeReturn24h || 0), 'en')}%)`}
+            valueColor={getColor(return24h)}
+          />
+          <StatLine
+            title="Total PnL"
+            value={`${getSign(
+              walletAssetDetails.pnl?.totalPnl
+            )}${formatCurrencyValue(
+              Math.abs(walletAssetDetails.pnl?.totalPnl || 0),
+              'en',
+              currency
+            )}`}
+            valueColor={getColor(walletAssetDetails.pnl?.totalPnl)}
+          />
+          <StatLine
+            title="Realised PnL"
+            value={`${getSign(
+              walletAssetDetails.pnl?.realizedPnl
+            )}${formatCurrencyValue(
+              Math.abs(walletAssetDetails.pnl?.realizedPnl || 0),
+              'en',
+              currency
+            )}`}
+            valueColor={getColor(walletAssetDetails.pnl?.realizedPnl)}
+          />
+          <StatLine
+            title="Unrealised PnL"
+            value={`${getSign(
+              walletAssetDetails.pnl?.unrealizedPnl
+            )}${formatCurrencyValue(
+              Math.abs(walletAssetDetails.pnl?.unrealizedPnl || 0),
+              'en',
+              currency
+            )} (${formatPercent(
+              Math.abs(walletAssetDetails.pnl?.relativeUnrealizedPnl || 0),
+              'en'
+            )}%)`}
+            valueColor={getColor(walletAssetDetails.pnl?.relativeUnrealizedPnl)}
+          />
+          <StatLine
+            title="Invested"
+            value={formatCurrencyValue(
+              walletAssetDetails.pnl?.bought || 0,
+              'en',
+              currency
+            )}
+          />
+          <StatLine
+            title="Average Cost"
+            value={formatCurrencyValue(
+              walletAssetDetails.pnl?.averageBuyPrice || 0,
+              'en',
+              currency
+            )}
+          />
+          <VStack gap={0}>
+            <UnstyledButton
+              className="parent-hover"
+              style={{
+                position: 'relative',
+                ['--parent-hovered-content-background-color' as string]:
+                  'var(--neutral-200)',
+              }}
+              onClick={() => setShowNetworkDistribution((prev) => !prev)}
+            >
+              <div
+                className="content-hover"
+                style={{
+                  position: 'absolute',
+                  inset: '-8px -12px',
+                  borderRadius: 12,
+                }}
+              />
+              <HStack
+                gap={12}
+                alignItems="center"
+                justifyContent="space-between"
+                style={{ position: 'relative' }}
+              >
+                <HStack gap={8} alignItems="center">
+                  <ChainsIcon style={{ width: 16, height: 16 }} />
+                  <UIText kind="body/regular">Network Distribution</UIText>
+                </HStack>
+                <ArrowDownIcon
+                  style={{
+                    width: 24,
+                    height: 24,
+                    transformOrigin: 'center',
+                    transitionDuration: '0.2s',
+                    transform: showNetworkDistribution
+                      ? 'rotate(180deg)'
+                      : 'rotate(0deg)',
+                  }}
+                />
+              </HStack>
+            </UnstyledButton>
+            <div
+              style={{
+                overflow: 'hidden',
+                maxHeight: showNetworkDistribution
+                  ? walletAssetDetails.chainsDistribution.length * 40
+                  : 0,
+                transition: 'max-height 0.3s ease-in-out',
+              }}
+            >
+              <VStack gap={16} style={{ paddingTop: 16 }}>
+                {walletAssetDetails.chainsDistribution.map(
+                  ({ chain, percentageAllocation, value }) => (
+                    <HStack
+                      key={chain.id}
+                      gap={12}
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <HStack gap={8} alignItems="center">
+                        <img
+                          src={chain.iconUrl}
+                          alt={chain.name}
+                          width={16}
+                          height={16}
+                        />
+                        <HStack gap={4} alignItems="center">
+                          <UIText kind="body/regular">{chain.name}</UIText>
+                          <UIText
+                            kind="caption/regular"
+                            color="var(--neutral-500)"
+                          >
+                            {`${middot} ${formatPercent(
+                              percentageAllocation,
+                              'en'
+                            )}%`}
+                          </UIText>
+                        </HStack>
+                      </HStack>
+                      <UIText kind="body/accent">
+                        {formatCurrencyValue(value, 'en', currency)}
+                      </UIText>
+                    </HStack>
+                  )
+                )}
+              </VStack>
+            </div>
           </VStack>
         </VStack>
         <Line />
@@ -153,7 +372,7 @@ export function AssetAddressDetails({
                         height={16}
                       />
                     ) : (
-                      <ChainsIcon />
+                      <ChainsIcon style={{ width: 16, height: 16 }} />
                     )}
                     <UIText kind="body/accent">
                       <NeutralDecimals
