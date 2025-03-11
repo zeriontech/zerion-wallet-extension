1ed484db0edb7fa9df4a18a69a9aaee1d9ddd837
Andrey <zertsalov96@yandex.ru>
2025-03-11 13:31:15 +0000

    Support test mode

--- src/modules/zerion-api/hooks/useAssetFullInfo.ts
+++ src/modules/zerion-api/hooks/useAssetFullInfo.ts
@@ -1,21 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { ZerionAPI } from 'src/modules/zerion-api/zerion-api.client';
 import { type Params } from '../requests/asset-get-fungible-full-info';
+import type { BackendSourceParams } from '../shared';
+
+const ONE_MINUTE = 1000 * 60;
 
 export function useAssetFullInfo(
   params: Params,
-  {
-    suspense = false,
-  }: {
-    suspense?: boolean;
-  } = {}
+  { source }: BackendSourceParams,
+  { suspense = false }: { suspense?: boolean } = {}
 ) {
   return useQuery({
-    queryKey: ['assetGetFungibleFullInfo', params],
-    queryFn: async () => {
-      return ZerionAPI.assetGetFungibleFullInfo(params);
-    },
+    queryKey: ['assetGetFungibleFullInfo', params, source],
+    queryFn: () => ZerionAPI.assetGetFungibleFullInfo(params, { source }),
     suspense,
-    staleTime: 1000 * 60,
+    staleTime: ONE_MINUTE,
   });
 }
--- src/modules/zerion-api/hooks/useWalletAssetPnl.ts
+++ src/modules/zerion-api/hooks/useWalletAssetPnl.ts
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import type { Params } from '../requests/asset-get-fungible-pnl';
 import { ZerionAPI } from '../zerion-api.client';
+import type { BackendSourceParams } from '../shared';
 
 const TEN_MINUTES = 1000 * 60 * 10;
 
-export function useAssetAddressPnl(
+export function useWalletAssetPnl(
   params: Params,
+  { source }: BackendSourceParams,
   {
     suspense = false,
     enabled = true,
@@ -15,8 +17,8 @@ export function useAssetAddressPnl(
   } = {}
 ) {
   return useQuery({
-    queryKey: ['assetGetFungiblePnl', params],
-    queryFn: () => ZerionAPI.assetGetFungiblePnl(params),
+    queryKey: ['assetGetFungiblePnl', params, source],
+    queryFn: () => ZerionAPI.assetGetFungiblePnl(params, { source }),
     suspense,
     enabled,
     staleTime: TEN_MINUTES,
