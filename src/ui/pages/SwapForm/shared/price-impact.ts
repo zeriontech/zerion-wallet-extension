3115d763dbb82a0ce7cfe525ba44a2eb7d4c9c68
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-03-20 01:48:52 +0300

    Add PriceImpact & getPriceImpact

--- src/ui/pages/SwapForm/shared/price-impact.ts
+++ src/ui/pages/SwapForm/shared/price-impact.ts
@@ -1,11 +1,32 @@
 import type BigNumber from 'bignumber.js';
 
-const PRICE_IMPACT_WARNING_THRESHOLD = -0.05;
+const LOW_THRESHOLD = -0.05;
+const HIGH_THRESHOLD = -0.15;
 
-export function exceedsPriceImpactThreshold({
+export enum PriceImpact {
+  Positive,
+  Low,
+  Medium,
+  High,
+}
+
+// x >= 0 => Positive
+// -0.05 <= x < 0 => Low
+// -0.15 <= x < -0.05 => Medium
+// x < -0.15 => High
+
+export function getPriceImpact({
   relativeChange,
 }: {
   relativeChange: BigNumber;
 }) {
-  return relativeChange.isLessThan(PRICE_IMPACT_WARNING_THRESHOLD);
+  if (relativeChange.isGreaterThanOrEqualTo(0)) {
+    return PriceImpact.Positive;
+  } else if (relativeChange.isGreaterThanOrEqualTo(LOW_THRESHOLD)) {
+    return PriceImpact.Low;
+  } else if (relativeChange.isGreaterThanOrEqualTo(HIGH_THRESHOLD)) {
+    return PriceImpact.Medium;
+  } else {
+    return PriceImpact.High;
+  }
 }
