536c2bf51db89bd58be6c9ecf8f363b972c04c1f
Andrey <zertsalov96@yandex.ru>
2025-03-25 15:48:18 +0000

    Update manifest and fix flow

---
+++ scripts/update-dev-manifest.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+const fs = require('fs/promises');
+const path = require('path');
+const manifest = require('../dist/manifest.json');
+
+const GIT_BRANCH_NAME = process.env.GIT_BRANCH_NAME;
+
+const dist = path.join(__dirname, '../dist');
+
+async function updateDevName() {
+  await fs.writeFile(
+    path.join(dist, 'manifest.json'),
+    JSON.stringify(
+      {
+        ...manifest,
+        name: 'Zerion Test Build',
+        description: `For the branch: ${GIT_BRANCH_NAME}`,
+      },
+      null,
+      2
+    )
+  );
+}
+
+updateDevName();
