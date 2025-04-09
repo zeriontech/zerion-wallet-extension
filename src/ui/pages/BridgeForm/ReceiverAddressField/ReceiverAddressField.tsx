1d8db6041712d6e7ba022b986aeab0449b607600
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-04-09 15:45:20 +0300

    Update field

--- src/ui/pages/BridgeForm/ReceiverAddressField/ReceiverAddressField.tsx
+++ src/ui/pages/BridgeForm/ReceiverAddressField/ReceiverAddressField.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HiddenValidationInput } from 'src/ui/shared/forms/HiddenValidationInput';
 import { HStack } from 'src/ui/ui-kit/HStack';
 import { Toggle } from 'src/ui/ui-kit/Toggle';
-import { VStack } from 'src/ui/ui-kit/VStack';
 import { UIText } from 'src/ui/ui-kit/UIText';
 import { AddressInputWrapper } from '../../SendForm/fieldsets/AddressInput';
 
@@ -22,29 +21,31 @@ export function ReceiverAddressField({
   onResolvedChange(value: string): void;
 }) {
   return (
-    <VStack gap={4}>
-      <HStack gap={8} justifyContent="space-between" alignItems="center">
-        <UIText kind="small/regular">Bridge and send to another address</UIText>
-        <Toggle
-          checked={showAddressInput}
-          onChange={(event) => onShowInputChange(event.currentTarget.checked)}
-        />
-      </HStack>
-      {showAddressInput ? (
-        <>
-          <HiddenValidationInput
-            customValidity={to ? '' : 'Cannot resolve recipient'}
-          />
-          <AddressInputWrapper
-            name="receiverAddressInput"
-            value={receiverAddressInput ?? ''}
-            required={true}
-            resolvedAddress={to ?? null}
-            onChange={onChange}
-            onResolvedChange={onResolvedChange}
-          />
-        </>
-      ) : null}
-    </VStack>
+    <>
+      <HiddenValidationInput
+        customValidity={to ? '' : 'Cannot resolve recipient'}
+      />
+      <AddressInputWrapper
+        fieldsetTitle={
+          <HStack gap={8} justifyContent="space-between" alignItems="center">
+            <UIText kind="small/regular">
+              Bridge and send to another address
+            </UIText>
+            <Toggle
+              checked={showAddressInput}
+              onChange={(event) =>
+                onShowInputChange(event.currentTarget.checked)
+              }
+            />
+          </HStack>
+        }
+        name="receiverAddressInput"
+        value={receiverAddressInput ?? ''}
+        required={true}
+        resolvedAddress={to ?? null}
+        onChange={onChange}
+        onResolvedChange={onResolvedChange}
+      />
+    </>
   );
 }
