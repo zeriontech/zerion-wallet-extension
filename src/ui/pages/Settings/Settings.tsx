c8931e06cdba0bb01088676c751f6c16863e322c
Andrey <zertsalov2910@gmail.com>
2025-04-12 14:32:56 +0100

    update

--- src/ui/pages/Settings/Settings.tsx
+++ src/ui/pages/Settings/Settings.tsx
@@ -522,7 +522,7 @@ function TouchId() {
             required={true}
           />
           <Button disabled={createPasswordMutation.isLoading}>
-            Create Passkey
+            Create Passkey 1
           </Button>
         </form>
         {createPasswordMutation.isLoading ? <CircleSpinner /> : null}
