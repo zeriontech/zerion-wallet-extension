ac0d732181f5a29f492603647311c21015c0ea15
everdimension <everdimension@gmail.com>
2025-05-01 11:20:20 +0300

    Improve types and type suggestions for Port Channel methods

--- src/ui/shared/channels.types.ts
+++ src/ui/shared/channels.types.ts
@@ -1,27 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import type { PortMessageChannel } from 'src/shared/PortMessageChannel';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AsyncMethod = (...args: any) => Promise<any>;
-
-export type RPCPort<Implementation> = Omit<PortMessageChannel, 'request'> & {
-  request<
-    T extends string,
-    Method = T extends keyof Implementation
-      ? Implementation[T] extends AsyncMethod
-        ? Implementation[T]
-        : never
+/**
+ * Extracts only async methods and simplifies signature, i.e.:
+ * type X = ExtractChannelMethods<{ fn1(params: { params: { a: string } }): Promise<number> }>
+ * Results in:
+ * type X = { fn1(params: { a: string } ): Promise<number> }
+ */
+export type ExtractChannelMethods<T> = {
+  [K in keyof T as K extends string
+    ? T[K] extends (...args: any[]) => Promise<any>
+      ? K
       : never
-  >(
-    method: Method extends AsyncMethod ? T : never,
-    ...params: Method extends AsyncMethod
-      ? Omit<Parameters<Method>[0], 'context'> extends {
-          params: unknown;
-        }
-        ? [
-            params: Omit<Parameters<Method>[0], 'context'>['params'],
-            id?: number
-          ]
-        : [params?: undefined, id?: number]
-      : [never]
-  ): Method extends AsyncMethod ? ReturnType<Method> : never;
+    : never]: T[K] extends (arg: infer Arg, ...args: any[]) => infer R
+    ? Arg extends { params: infer P }
+      ? (params: P) => R
+      : () => R
+    : never;
 };
+
+/**
+ * Turns methods into an rpc-like "request" signature:
+ * type X = RPCApi<{ fn1(params: { a: string } ): Promise<number> }>
+ * Results in:
+ * type X = { request: (method: 'fn1', params: { a: string }) => Promise<number> }
+ */
+export type RPCApi<T> = {
+  request<K extends keyof T>(
+    ...args: T[K] extends () => Promise<infer _>
+      ? [method: K]
+      : T[K] extends (params: infer P) => Promise<infer _>
+      ? [method: K, params: P]
+      : never
+  ): T[K] extends (...args: any[]) => Promise<infer R> ? Promise<R> : never;
+};
+
+export type RPCPort<Implementation> = Omit<PortMessageChannel, 'request'> &
+  RPCApi<ExtractChannelMethods<Implementation>>;
