d3dfff096b506486319977201278ddd3ad9cb251
Andrey <zertsalov2910@gmail.com>
2025-07-01 09:22:40 +0100

    Update execAsync

--- scripts/build-firefox.js
+++ scripts/build-firefox.js
@@ -4,13 +4,26 @@ const { execAsync } = require('./execAsync');
 async function build() {
   console.log('Building Firefox'); // eslint-disable-line no-console
 
-  await execAsync('rm -rf .parcel-cache-firefox dist-firefox');
-  await execAsync('cp ./src/manifest-firefox.json ./src/manifest.json');
+  await execAsync('rm', ['-rf', '.parcel-cache-firefox', 'dist-firefox']);
+  await execAsync('cp', ['./src/manifest-firefox.json', './src/manifest.json']);
+  let env = Object.assign({}, process.env, { PLATFORM: 'firefox' });
   await execAsync(
     // pass --no-content-hash flag to avoid code-splitting issues:
     // https://github.com/parcel-bundler/parcel/issues/8071#issuecomment-1214438848
     // https://github.com/parcel-bundler/parcel/issues/8071#issuecomment-1133549719
-    'PLATFORM=firefox ./node_modules/.bin/parcel build src/manifest.json src/ui/hardware-wallet/ledger.html --no-content-hash --no-source-maps --dist-dir dist-firefox --cache-dir .parcel-cache-firefox'
+    './node_modules/.bin/parcel',
+    [
+      'build',
+      'src/manifest.json',
+      'src/ui/hardware-wallet/ledger.html',
+      '--no-content-hash',
+      '--no-source-maps',
+      '--dist-dir',
+      'dist-firefox',
+      '--cache-dir',
+      '.parcel-cache-firefox',
+    ],
+    { env }
   );
 
   console.log('Done Firefox'); // eslint-disable-line no-console
--- scripts/build.js
+++ scripts/build.js
@@ -5,19 +5,22 @@ async function build() {
   console.log('Building Chrome'); // eslint-disable-line no-console
 
   await execAsync('rm', ['-rf', '.parcel-cache', 'dist']);
-  await execAsync('cp' , ['./src/manifest-chrome.json', './src/manifest.json']);
+  await execAsync('cp', ['./src/manifest-chrome.json', './src/manifest.json']);
+
+  let env = Object.assign({}, process.env, { PLATFORM: 'chrome' });
   await execAsync(
     // pass --no-content-hash flag to avoid code-splitting issues:
     // https://github.com/parcel-bundler/parcel/issues/8071#issuecomment-1214438848
     // https://github.com/parcel-bundler/parcel/issues/8071#issuecomment-1133549719
-    'PLATFORM=chrome ./node_modules/.bin/parcel',
+    './node_modules/.bin/parcel',
     [
       'build',
       'src/manifest.json',
       'src/ui/hardware-wallet/ledger.html',
       '--no-content-hash',
       '--no-source-maps',
-    ]
+    ],
+    { env }
   );
 
   console.log('Done Chrome'); // eslint-disable-line no-console
