b94d63d6e67bb51133f863be8d342dc0f9d8f2f6
Andrey <zertsalov2910@gmail.com>
2025-08-06 17:17:21 +0100

    fix

--- src/ui/pages/Search/useSearchQuery.ts
+++ src/ui/pages/Search/useSearchQuery.ts
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { ZerionAPI } from 'src/modules/zerion-api/zerion-api.client';
 import type { Response } from 'src/modules/zerion-api/requests/search-query';
 
-export function useSearchQuery(query: string, currency: string) {
+export function useSearchQuery(query: string, currency: string, limit = 5) {
   return useQuery<Response | null>({
-    queryKey: ['searchQuery', query, currency],
+    queryKey: ['searchQuery', query, currency, limit],
     queryFn: () => {
       return ZerionAPI.searchQuery({
         query,
         currency,
-        limit: 5,
+        limit,
       });
     },
     enabled: query.trim().length > 0,
