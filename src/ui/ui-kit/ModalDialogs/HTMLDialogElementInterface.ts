51546f7db146bec7f26dfba3d22ffb786b328eb2
Andrey <zertsalov2910@gmail.com>
2025-08-21 11:53:50 +0100

    remove

--- src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface.ts
+++ src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface.ts
@@ -2,7 +2,6 @@ export interface HTMLDialogElementInterface extends HTMLElement {
   showModal(): void;
   close(): void;
   show(): void;
-  requestClose(): void;
   open: boolean;
   returnValue: string;
 }
