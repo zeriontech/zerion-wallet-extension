9b95df711c081dd5561856761a0b4f267a07e125
everdimension <everdimension@gmail.com>
2025-10-22 14:31:43 +0300

    Update ActionButtonsRow

--- src/ui/pages/Overview/ActionButtonsRow/ActionButtonsRow.tsx
+++ src/ui/pages/Overview/ActionButtonsRow/ActionButtonsRow.tsx
@@ -1,13 +1,13 @@
 import browser from 'webextension-polyfill';
 import type { ComponentPropsWithoutRef, ElementType } from 'react';
-import React from 'react';
+import React, { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import classNames from 'classnames';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import SwapIcon from 'jsx:src/ui/assets/actions/swap.svg';
 import SendIcon from 'jsx:src/ui/assets/actions/send.svg';
-import ReceiveIcon from 'jsx:src/ui/assets/actions/receive.svg';
 import BridgeIcon from 'jsx:src/ui/assets/actions/bridge.svg';
-import BuyIcon from 'jsx:src/ui/assets/actions/buy.svg';
+import FundIcon from 'jsx:src/ui/assets/actions/fund.svg';
 import { UnstyledAnchor } from 'src/ui/ui-kit/UnstyledAnchor';
 import { walletPort } from 'src/ui/shared/channels';
 import { UnstyledLink } from 'src/ui/ui-kit/UnstyledLink';
@@ -16,7 +16,13 @@ import { HStack } from 'src/ui/ui-kit/HStack';
 import { UIText } from 'src/ui/ui-kit/UIText';
 import { Button } from 'src/ui/ui-kit/Button';
 import { WithMainnetOnlyWarningDialog } from 'src/ui/features/testnet-mode/MainnetOnlyWarningDialog';
+import { type HTMLDialogElementInterface } from 'src/ui/ui-kit/ModalDialogs/HTMLDialogElementInterface';
+import { UnstyledButton } from 'src/ui/ui-kit/UnstyledButton';
+import { emitter } from 'src/ui/shared/events';
+import { AddFundsOptionsDialog } from '../../Receive/AddFundsOptionsDialog';
 import * as s from './styles.module.css';
+
+const ZERION_ORIGIN = 'https://app.zerion.io';
 
 function ActionButton<As extends ElementType = 'a'>({
   as,
@@ -39,52 +45,50 @@ function ActionButton<As extends ElementType = 'a'>({
   );
 }
 
-const ZERION_ORIGIN = 'https://app.zerion.io';
+function acceptOrigin(params: { address: string; origin: string }) {
+  return walletPort.request('acceptOrigin', params);
+}
+
+export function useOpenAndConnectToZerion({ address }: { address: string }) {
+  const { data: activeTabs } = useQuery({
+    queryKey: ['browser/activeTab'],
+    queryFn: () => browser.tabs.query({ active: true, currentWindow: true }),
+  });
+  const activeTab = activeTabs ? activeTabs[0] : null;
+  const handleAnchorClick = (event: React.MouseEvent<HTMLElement>) => {
+    acceptOrigin({ origin: ZERION_ORIGIN, address });
+    const href = event.currentTarget.getAttribute('href');
+    const activeTabUrl = activeTab?.url ? new URL(activeTab.url) : null;
+    if (href && activeTab && activeTabUrl?.origin == ZERION_ORIGIN) {
+      event.preventDefault();
+      browser.tabs.update(activeTab.id, { url: href });
+    }
+  };
+  return { handleAnchorClick };
+}
 
 export function ActionButtonsRow() {
+  const { pathname } = useLocation();
   const { data: wallet } = useQuery({
     queryKey: ['wallet/uiGetCurrentWallet'],
     queryFn: () => {
       return walletPort.request('uiGetCurrentWallet');
     },
   });
-  const { mutate: acceptOrigin } = useMutation({
-    mutationFn: async ({
-      address,
-      origin,
-    }: {
-      address: string;
-      origin: string;
-    }) => {
-      return walletPort.request('acceptOrigin', { origin, address });
-    },
-  });
   const addWalletParams = useWalletParams(wallet);
+  const fundOptionsDialogRef = useRef<HTMLDialogElementInterface>(null);
 
-  const { data: activeTabs } = useQuery({
-    queryKey: ['browser/activeTab'],
-    queryFn: () => browser.tabs.query({ active: true, currentWindow: true }),
-  });
-  const activeTab = activeTabs ? activeTabs[0] : null;
-  const { mutate: updateTab } = useMutation({
-    mutationFn: async ({ tab, url }: { tab: browser.Tabs.Tab; url: string }) =>
-      browser.tabs.update(tab.id, { url }),
-  });
   if (!addWalletParams || !wallet) {
     return null;
   }
-  const performAction = (event: React.MouseEvent<HTMLElement>) => {
-    acceptOrigin({ origin: ZERION_ORIGIN, address: wallet.address });
-    const href = event.currentTarget.getAttribute('href');
-    const activeTabUrl = activeTab?.url ? new URL(activeTab.url) : null;
-    if (href && activeTab && activeTabUrl?.origin == ZERION_ORIGIN) {
-      event.preventDefault();
-      updateTab({ tab: activeTab, url: href });
-    }
-  };
 
   return (
     <div className={s.containerRoot}>
+      <AddFundsOptionsDialog
+        dialogRef={fundOptionsDialogRef}
+        wallet={wallet}
+        analytics={{ pathname, address: wallet.address }}
+      />
       <ul
         className={s.list}
         style={{
@@ -95,18 +99,27 @@ export function ActionButtonsRow() {
       >
         <li>
           <ActionButton
-            title="Send"
-            as={UnstyledLink}
-            icon={<SendIcon />}
-            to="/send-form"
+            title="Receive"
+            as={UnstyledButton}
+            icon={<FundIcon />}
+            onClick={() => {
+              fundOptionsDialogRef.current?.showModal();
+
+              emitter.emit('buttonClicked', {
+                buttonName: 'Fund',
+                buttonScope: 'General',
+                pathname,
+                walletAddress: wallet.address,
+              });
+            }}
           />
         </li>
         <li>
           <ActionButton
-            title="Receive"
+            title="Send"
             as={UnstyledLink}
-            icon={<ReceiveIcon />}
-            to={`/receive?address=${wallet.address}`}
+            icon={<SendIcon />}
+            to="/send-form"
           />
         </li>
         <li>
@@ -123,16 +136,6 @@ export function ActionButtonsRow() {
                 }}
               />
             )}
-          />
-        </li>
-        <li>
-          <ActionButton
-            title="Buy"
-            icon={<BuyIcon />}
-            href={`${ZERION_ORIGIN}/deposit?${addWalletParams}`}
-            onClick={performAction}
-            target="_blank"
-            rel="noopener noreferrer"
           />
         </li>
         <li>
