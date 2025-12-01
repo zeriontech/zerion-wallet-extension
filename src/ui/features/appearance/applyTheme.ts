e6ad3e63137f496777ab40c0cdd5aaa22a6e0f14
Andrey <zertsalov2910@gmail.com>
2025-12-01 20:43:07 +0000

    Preserve system theme when open UI (#873)

--- src/ui/features/appearance/applyTheme.ts
+++ src/ui/features/appearance/applyTheme.ts
@@ -4,7 +4,12 @@ import { Theme } from './Theme';
 export function applyTheme(theme: Theme) {
   if (theme === Theme.dark) {
     document.documentElement.classList.add(s['theme-dark']);
+    document.documentElement.style.setProperty(
+      '--default-background',
+      '#16161a'
+    );
   } else {
     document.documentElement.classList.remove(s['theme-dark']);
+    document.documentElement.style.setProperty('--default-background', 'white');
   }
 }
