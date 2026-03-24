b5578f71eab6b831ec519e25253132a65f42b6e1
Andrey <zertsalov2910@gmail.com>
2026-03-24 12:21:39 +0000

    Fix sign button visibility during signing

--- src/ui/pages/SwapForm/SwapForm.tsx
+++ src/ui/pages/SwapForm/SwapForm.tsx
@@ -1066,7 +1066,18 @@ function SwapFormComponent() {
           needsManualSign={Boolean(wallet && isDeviceAccount(wallet))}
         />
         {wallet && globalPreferences && approveAndTradeInOneAction ? (
-          <div style={{ position: 'absolute', top: '100%' }}>
+          <div
+            style={{
+              border: 0,
+              clip: 'rect(0 0 0 0)',
+              height: '1px',
+              margin: '-1px',
+              overflow: 'hidden',
+              padding: 0,
+              position: 'absolute',
+              width: '1px',
+            }}
+          >
             <SignTransactionButton
               ref={sendTxBtnRef}
               wallet={wallet}
